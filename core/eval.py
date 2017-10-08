38b7f6e5cafddaa6cf0b69d143e0c7e5dfde2995
Nicolay Rusnachenko <kolyarus@yandex.ru>
2017-10-08 12:18:28 +0400

    Fixed issues in eval.py

--- core/eval.py
+++ core/eval.py
@@ -25,11 +25,6 @@ syn_dict = [
     ['Башар Асад', 'Асад'],\
     ]
 
-""" Округление до 4 знаков после запятой.
-"""
-def round4(numb):
-    return int(numb * 10000) / 10000
-
 """ Проверка имен на заменяемость. Имена могут быть:
 - подстрокой другого: Путин vs Владимир Путин, Валентин vs Валентино
 - формой слова: соединенные vs соединенный vs соединить.
@@ -73,29 +68,29 @@ def checkWords(word1, word2):
 """
 def calcPrecisionAndRecall(results):
     # Берем все позитивные и негативные ответы команд
-    pos_answers=results[(results['how_results']=='pos')]
-    neg_answers=results[(results['how_results']=='neg')]
+    pos_answers=results[(results['how_results'] == 'pos')]
+    neg_answers=results[(results['how_results'] == 'neg')]
 
     # Расчет точности.
-    if len(pos_answers)!=0:
-        pos_prec=len(pos_answers[(pos_answers['comparison']==True)])/ len(pos_answers)
+    if len(pos_answers) != 0:
+        pos_prec = len(pos_answers[(pos_answers['comparison']==True)])/ len(pos_answers)
     else:
-        pos_prec=0
-    if len(neg_answers)!=0:
-        neg_prec=len(neg_answers[(neg_answers['comparison']==True)])/ len(neg_answers)
+        pos_prec = 0
+    if len(neg_answers) != 0:
+        neg_prec = len(neg_answers[(neg_answers['comparison']==True)])/ len(neg_answers)
     else:
-        neg_prec=0
+        neg_prec = 0
 
     # Расчет полноты.
-    if len(results[results['how_orig']=='pos'])!=0:
-        pos_recall=len(pos_answers[(pos_answers['comparison']==True)])/ len(results[results['how_orig']=='pos'])
+    if len(results[results['how_orig'] == 'pos']) != 0:
+        pos_recall = len(pos_answers[(pos_answers['comparison'] == True)]) / len(results[results['how_orig'] == 'pos'])
     else:
-        pos_recall=0
-    if len(results[results['how_orig']=='neg'])!=0:
-        neg_recall=len(neg_answers[(neg_answers['comparison']==True)])/ len(results[results['how_orig']=='neg'])
+        pos_recall = 0
+    if len(results[results['how_orig'] == 'neg']) != 0:
+        neg_recall = len(neg_answers[(neg_answers['comparison'] == True)]) / len(results[results['how_orig'] == 'neg'])
     else:
-        neg_recall=0
-    return pos_prec, neg_prec, pos_recall, neg_recall
+        neg_recall = 0
+    return float(pos_prec), float(neg_prec), float(pos_recall), float(neg_recall)
 
 """ Расчет данных для файла.
 """
@@ -103,9 +98,8 @@ def calc_a_file(num):
     # Если файл существует.
     filename = "test_{}/art{}.opin.txt".format(color, str(num))
     if not os.path.exists(filename):
-        print filename
         print "-  art" + str(num) + ".opin.txt" # , end=" ")
-        return
+        return 0, 0, 0, 0
 
     # Считываем файл ответов команды и отбрасываем лишнюю информацию.
     # print("test_"+color+"/art"+str(num)+".opin.txt", end=" ")
@@ -122,9 +116,9 @@ def calc_a_file(num):
 
     # Считываем файл ответов экспертов.
     file_experts = "test_orig/artest_orig/art{}.opin.txt".format(str(num))
-    print(file_experts)
+    print "reading: {}".format(file_experts)
     file2 = pd.read_csv(file_experts, sep=',', header=None)
-    test_file = file2[[0,1,2]].copy()
+    test_file = file2[[0, 1, 2]].copy()
     test_file.columns=['who', 'to', 'how_results']
     test_file['who'] = test_file['who'].str.strip()
     test_file['who'] = test_file['who'].str.lower()
@@ -134,7 +128,6 @@ def calc_a_file(num):
 
     orig_file=orig_file.sort_values(['who', 'to'])
     test_file=test_file.sort_values(['who', 'to'])
-
     # Сливаем ответы там, где имена собвпадают.
     results= test_file.merge(orig_file, 'outer', on=['who','to'], copy=False)
     results.insert(len(results.columns), 'comparison', '')
@@ -146,6 +139,7 @@ def calc_a_file(num):
     count_res=len(faulty)-len(faulty[faulty.how_orig.isnull()])
 
     # Идем по всем ответам, сравниваем всех со всеми, может быть имя было выделено командой не так, или эксперт написал синоним.
+    has_changes = False
     for i in range(count_res, len(faulty)):
         for j in range(count_res):
             if(checkWords(results['who'][results.index[i]], results['who'][results.index[j]]) and \
@@ -156,12 +150,15 @@ def calc_a_file(num):
                 results.loc[results.index[i], ('how_orig')] = results.loc[results.index[j], ('how_orig')]
                 results.loc[results.index[i], ('comparison')] = (results.loc[results.index[i], ('how_orig')] == results.loc[results.index[i], ('how_results')])
                 results.loc[results.index[j], ('comparison')] = '---'
+                has_changes = True
+
 
     # Выкидываем все строки, которые были слиты вместе с другими.
-    results=results[results['comparison'] != '---']
+    if has_changes:
+        results = results[results['comparison'] != '---']
     # print(">test_"+color+"/art"+str(num)+".comp.txt")
     # Сохраняем файл со сравнениями.
-    comarison_file = "test_{}/art{}.comp.txt".format(color, str(num))
+    comparison_file = "test_{}/art{}.comp.txt".format(color, str(num))
     results.to_csv(comparison_file)
 
     return calcPrecisionAndRecall(results)
@@ -173,19 +170,19 @@ if __name__ == "__main__":
     pos_prec, neg_prec, pos_recall, neg_recall=(0,0,0,0)
 
     for n in range(46, 76):
-        try:
-            [pos_prec1, neg_prec1, pos_recall1, neg_recall1] = calc_a_file(n)
-            pos_prec += pos_prec1
-            neg_prec += neg_prec1
-            pos_recall += pos_recall1
-            neg_recall += neg_recall1
-        except BaseException:
-            pass
+        [pos_prec1, neg_prec1, pos_recall1, neg_recall1] = calc_a_file(n)
+
+        pos_prec += pos_prec1
+        neg_prec += neg_prec1
+        pos_recall += pos_recall1
+        neg_recall += neg_recall1
 
     pos_prec /= 30
     neg_prec /= 30
     pos_recall /= 30
     neg_recall /= 30
+
+
     if pos_prec * pos_recall != 0:
         f1_pos = 2 * pos_prec*pos_recall / (pos_prec+pos_recall)
     else:
@@ -195,7 +192,7 @@ if __name__ == "__main__":
     else:
         f1_neg = 0
 
-    print round4(pos_prec), ';', round4(neg_prec), ';', \
-        round4(pos_recall), ';', round4(neg_recall), ';', \
-        round4(f1_pos), ';', round4(f1_neg), ';', \
-        round4((f1_pos + f1_neg) / 2)
+    print "pos_prec: %.4f, neg_prec: %.4f" % (pos_prec, neg_prec)
+    print "pos_recall: %.4f, neg_recall: %.4f" % (pos_recall, neg_recall)
+    print "f1_pos: %.4f, f1_neg: %.4f" % (f1_pos, f1_neg)
+    print "f1: %.4f" % ((f1_pos + f1_neg) / 2)
