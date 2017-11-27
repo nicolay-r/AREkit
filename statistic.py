78b49bbb111ceec90fb78ab628c2a0b9eb1982c1
Nicolay Rusnachenko <kolyarus@yandex.ru>
2017-11-28 00:10:05 +0400

    Statistic errors were fixed

--- statistic.py
+++ statistic.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 from core.source.opinion import OpinionCollection
-from core.labels import PositiveLabel, NegativeLabel
+from core.labels import Label, PositiveLabel, NegativeLabel
 
 
 class MethodStatistic:
@@ -10,9 +10,16 @@ class MethodStatistic:
     def founded_opins(test_opins, etalon_opins, sentiment=None):
         assert(isinstance(test_opins, OpinionCollection))
         assert(isinstance(etalon_opins, OpinionCollection))
+        assert(isinstance(sentiment, Label) or sentiment is None)
+
         founded = 0
-        for e in test_opins:
-            founded += 1 if etalon_opins.has_opinion_by_synonyms(e, sentiment) else 0
+        for opin in test_opins:
+            if sentiment is not None:
+                if sentiment != opin.sentiment:
+                    continue
+            if etalon_opins.has_opinion_by_synonyms(opin, sentiment):
+                founded += 1
+
         return founded
 
     @staticmethod
@@ -26,7 +33,6 @@ class MethodStatistic:
             eo_filepath = "{}/art{}.opin.txt".format(etalon_root, n)
             to_filepath = "{}/art{}.opin.txt".format(test_root, n)
 
-
             test_opins = OpinionCollection.from_file(to_filepath, synonyms_filepath)
             etalon_opins = OpinionCollection.from_file(eo_filepath, synonyms_filepath)
 
@@ -39,11 +45,9 @@ class MethodStatistic:
 
         df.loc['sum'] = [float(df[c].sum()) for c in columns]
 
-        df.loc['found_all'] = None
-        df.loc['found_pos'] = None
-        df.loc['found_neg'] = None
-        df.loc['found_all'][0] = float(df.loc['sum']['t_all']) / df.loc['sum']['e_all']
-        df.loc['found_pos'][1] = float(df.loc['sum']['t_pos']) / df.loc['sum']['e_pos']
-        df.loc['found_neg'][2] = float(df.loc['sum']['t_neg']) / df.loc['sum']['e_neg']
+        df.loc['found'] = None
+        df.loc['found']['t_all'] = float(df.loc['sum']['t_all']) / df.loc['sum']['e_all']
+        df.loc['found']['t_pos'] = float(df.loc['sum']['t_pos']) / df.loc['sum']['e_pos']
+        df.loc['found']['t_neg'] = float(df.loc['sum']['t_neg']) / df.loc['sum']['e_neg']
 
         return df
