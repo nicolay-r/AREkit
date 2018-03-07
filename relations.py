dd9d1b77752eed94d41c3f573179bf8eea9abfd1
Nicolay Rusnachenko <kolyarus@yandex.ru>
2018-03-07 19:03:07 +0000

    Relation now includes a collection. The latter allows to initialize relations from opinion of certain news.

--- relations.py
+++ relations.py
@@ -1,4 +1,32 @@
 from core.source.news import News
+from core.source.opinion import Opinion
+
+
+class RelationCollection:
+
+    def __init__(self):
+        pass
+
+    def from_news_opinion(news, opinion, synonyms):
+
+        def get_appropriate_entities_value(value):
+            if synonyms.has_synonym(value):
+                return filter(
+                    lambda s: entities.has_entity_by_value(s),
+                    synonyms.get_synonyms_list(value))
+
+            elif entities.has_entity_by_value(value):
+                return [value]
+            else:
+                return []
+
+        assert(isinstance(news, News))
+        assert(isinstance(opinion, Opinion))
+        # TODO. Create method in core that provides all pairs of entities
+        # related to a Opinion. Widely used.
+
+    def __iter__(self):
+        pass
 
 
 class Relation:
