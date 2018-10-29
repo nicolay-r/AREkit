6efdfbf2091491a9df6c1c672f0397ec060ae111
Nicolay Rusnachenko <kolyarus@yandex.ru>
2018-10-29 15:20:40 +0300

    added raw processing and combination with docker

--- processing/syntax/syntaxnet_wrap.py
+++ processing/syntax/syntaxnet_wrap.py
@@ -1,3 +1,7 @@
+import socket
+import sys
+
+
 class SyntaxNetParserWrapper:
     """
     Interface
@@ -8,11 +12,59 @@ class SyntaxNetParserWrapper:
     def __init__(self):
         pass
 
-    def parse(self, text, debug=False):
-        """
-        return: SyntaxAnalysisResult
-        """
+    def parse(self, text, sentences=None, raw_output=False, debug=False):
+        raw_input_s = self._prepare_raw_input_for_syntaxnet(text,
+                                                            sentences)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host, self.port))
+        sock.sendall(raw_input_s)
+        raw_output_s = self._read_all_from_socket(sock)
+        sock.close()
+
+        if not raw_output_s:
+            return None
+
+        if raw_output:
+            return raw_output_s
+
+        # TODO: add output processing.
         pass
+
+    def _prepare_raw_input_for_syntaxnet(self, text, sentences):
+        """
+        (C) IINemo
+        Original: https://github.com/IINemo/syntaxnet_wrapper
+        """
+        raw_input_s = u''
+        if not sentences:
+            raw_input_s = text + u'\n\n'
+        else:
+            for sent in sentences:
+                line = u' '.join((text[e.begin: e.end] for e in sent))
+                raw_input_s += line
+                raw_input_s += u'\n'
+            raw_input_s += u'\n'
+
+        return raw_input_s.encode('utf8')
+
+    def _read_all_from_socket(self, sock):
+        """
+        (C) IINemo
+        Original: https://github.com/IINemo/syntaxnet_wrapper
+        """
+        buf = str()
+
+        try:
+            while True:
+                data = sock.recv(51200)
+                if data:
+                    buf += data
+                else:
+                    break
+        except socket.error as err:
+            print >> sys.stderr, 'Err: Socket error: ', err
+
+        return buf
 
 
 class PennPOSTags:
