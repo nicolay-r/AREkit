412379d5760d944072f29ce653af11d88692a6d4
Nicolay Rusnachenko <kolyarus@yandex.ru>
2018-12-31 21:35:24 +0300

    Fixed issue for token embeddding

--- source/embeddings/tokens.py
+++ source/embeddings/tokens.py
@@ -7,43 +7,43 @@ class TokenEmbeddingVectors:
     Embedding vectors for text punctuation, based on Tokens
     """
 
-    tokens = [Tokens.COMMA,
-              Tokens.COLON,
-              Tokens.SEMICOLON,
-              Tokens.QUOTE,
-              Tokens.DASH,
-              Tokens.EXC_SIGN,
-              Tokens.QUESTION_SIGN,
-              Tokens.OPEN_BRACKET,
-              Tokens.CLOSED_BRACKET,
-              Tokens.LONG_DASH,
-              Tokens.NUMBER,
-              Tokens.TRIPLE_DOTS,
-              Tokens.UNKNOWN_CHAR,
-              Tokens.UNKNOWN_WORD]
+    token_values = [Tokens.COMMA,
+                    Tokens.COLON,
+                    Tokens.SEMICOLON,
+                    Tokens.QUOTE,
+                    Tokens.DASH,
+                    Tokens.EXC_SIGN,
+                    Tokens.QUESTION_SIGN,
+                    Tokens.OPEN_BRACKET,
+                    Tokens.CLOSED_BRACKET,
+                    Tokens.LONG_DASH,
+                    Tokens.NUMBER,
+                    Tokens.TRIPLE_DOTS,
+                    Tokens.UNKNOWN_CHAR,
+                    Tokens.UNKNOWN_WORD]
 
     def __init__(self, vector_size):
         self.E = {}
-        for index, token in enumerate(self.tokens):
-            self.E[token] = get_random_vector(vector_size, index)
+        for index, token_value in enumerate(self.token_values):
+            self.E[token_value] = get_random_vector(vector_size, index)
 
     @staticmethod
     def count():
-        return len(TokenEmbeddingVectors.tokens)
+        return len(TokenEmbeddingVectors.token_values)
 
     @staticmethod
-    def get_token_index(token):
-        assert(isinstance(token, Token))
-        return TokenEmbeddingVectors.tokens.index(token.get_token_value())
+    def get_token_index(token_value):
+        assert(isinstance(token_value, unicode))
+        return TokenEmbeddingVectors.token_values.index(token_value)
 
-    def __getitem__(self, token):
-        assert (isinstance(token, unicode))
-        return self.E[token]
+    def __getitem__(self, token_value):
+        assert (isinstance(token_value, unicode))
+        return self.E[token_value]
 
     def __iter__(self):
-        for token in self.E:
-            yield token
+        for token_value in self.E:
+            yield token_value
 
-    def __contains__(self, token):
-        assert (isinstance(token, unicode))
-        return token in self.E
+    def __contains__(self, token_value):
+        assert (isinstance(token_value, unicode))
+        return token_value in self.E
