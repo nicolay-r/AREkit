7449098942edf4f962313c03a0b279018cc18753
Nicolay Rusnachenko <kolyarus@yandex.ru>
2019-04-09 13:14:37 +0300

    Added runtime object

---
+++ runtime/object.py
@@ -0,0 +1,50 @@
+from core.common.bound import Bound
+
+
+class TextObject:
+    """
+    Result of NER or found by lexicon (adj + noun or noun)
+    """
+
+    class ObjectTypes:
+        NamedEntity = "NE"
+        LexiconUnit = "LEX"
+
+    def __init__(self, terms, position, object_type=None):
+        assert(isinstance(terms, list))
+        assert(isinstance(position, int))
+        self.__terms = terms
+        self.__position = position
+        self.__obj_type = object_type
+
+    @property
+    def Position(self):
+        return self.__position
+
+    @classmethod
+    def create_as_named_entity(cls, terms, position):
+        return cls(terms, position, object_type=cls.ObjectTypes.NamedEntity)
+
+    @classmethod
+    def create_as_lexicon_unit(cls, terms, position):
+        return cls(terms, position, object_type=cls.ObjectTypes.LexiconUnit)
+
+    @property
+    def ObjectType(self):
+        return self.__obj_type
+
+    def get_value(self):
+        return u' '.join(self.__terms)
+
+    def is_named_entity(self):
+        return self.__obj_type == self.ObjectTypes.LexiconUnit
+
+    def get_bound(self):
+        return Bound(self.__position, len(self.__terms))
+
+    def iter_terms(self):
+        for term in self.__terms:
+            yield term
+
+    def __len__(self):
+        return len(self.__terms)
