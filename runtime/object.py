982a750863b789f030f7c972a76f5c6654be064c
Nicolay Rusnachenko <kolyarus@yandex.ru>
2019-04-13 12:43:45 +0300

    updated, removed unused code

--- runtime/object.py
+++ runtime/object.py
@@ -1,21 +1,13 @@
 from core.common.bound import Bound
 
 
-class TextObject:
-    """
-    Result of NER or found by lexicon (adj + noun or noun)
-    """
+class TextObject(object):
 
-    class ObjectTypes:
-        NamedEntity = "NE"
-        LexiconUnit = "LEX"
-
-    def __init__(self, terms, position, object_type=None):
+    def __init__(self, terms, position):
         assert(isinstance(terms, list))
         assert(isinstance(position, int))
         self.__terms = terms
         self.__position = position
-        self.__obj_type = object_type
         self.__tag = None
 
     @property
@@ -26,26 +18,11 @@ class TextObject:
     def Tag(self):
         return self.__tag
 
-    @property
-    def ObjectType(self):
-        return self.__obj_type
-
-    @classmethod
-    def create_as_named_entity(cls, terms, position):
-        return cls(terms, position, object_type=cls.ObjectTypes.NamedEntity)
-
-    @classmethod
-    def create_as_lexicon_unit(cls, terms, position):
-        return cls(terms, position, object_type=cls.ObjectTypes.LexiconUnit)
-
     def set_tag(self, value):
         self.__tag = value
 
     def get_value(self):
         return u' '.join(self.__terms)
-
-    def is_named_entity(self):
-        return self.__obj_type == self.ObjectTypes.LexiconUnit
 
     def get_bound(self):
         return Bound(self.__position, len(self.__terms))
