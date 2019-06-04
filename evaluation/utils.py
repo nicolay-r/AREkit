2f6eae87e84efcbf083effa00b8a62b64a395c96
Nicolay Rusnachenko <kolyarus@yandex.ru>
2019-06-04 11:00:12 +0300

    Added helper

--- evaluation/utils.py
+++ evaluation/utils.py
@@ -1,12 +1,29 @@
+import collections
+
+
+class FilesToCompareUtils:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_list_of_comparable_files(test_filepath_func, etalon_filepath_func, indices):
+        assert(isinstance(indices, collections.Iterable))
+
+        return [FilesToCompare(test_filepath_func=test_filepath_func,
+                               etalon_filepath_func=etalon_filepath_func,
+                               index=index)
+                for index in indices]
+
+
 class FilesToCompare:
 
-    # TODO. Provide functions instead
-    def __init__(self, test_filepath, etalon_filepath, index):
-        assert(isinstance(test_filepath, unicode))
-        assert(isinstance(etalon_filepath, unicode))
+    def __init__(self, test_filepath_func, etalon_filepath_func, index):
+        assert(callable(test_filepath_func))
+        assert(callable(etalon_filepath_func))
         assert(isinstance(index, int))
-        self.__test_fp = test_filepath
-        self.__etalon_fp = etalon_filepath
+        self.__test_fp = test_filepath_func(index)
+        self.__etalon_fp = etalon_filepath_func(index)
         self.__index = index
 
     @property
