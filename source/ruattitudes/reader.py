6533018ec04ad3a7649480811bed20874c03548e
Nicolay Rusnachenko <kolyarus@yandex.ru>
2019-06-09 08:17:26 +0300

    Name changed

--- source/ruattitudes/reader.py
+++ source/ruattitudes/reader.py
@@ -8,8 +8,7 @@ from core.source.ruattitudes.news import ProcessedNews
 from core.source.ruattitudes.sentence import ProcessedSentence
 
 
-# TODO. Rename as RuAttitudesFormatReader.
-class ContextsReader(object):
+class RuAttitudesFormatReader(object):
 
     NEWS_SEP_KEY = u'--------'
     FILE_KEY = u"Файл:"
@@ -45,37 +44,37 @@ class ContextsReader(object):
             for line in input.readlines():
                 line = line.decode('utf-8')
 
-                if ContextsReader.FILE_KEY in line:
+                if RuAttitudesFormatReader.FILE_KEY in line:
                     pass
 
-                if ContextsReader.OBJ_KEY in line:
-                    object = ContextsReader.__parse_object(line)
+                if RuAttitudesFormatReader.OBJ_KEY in line:
+                    object = RuAttitudesFormatReader.__parse_object(line)
                     objects_list.append(object)
 
-                if ContextsReader.OPINION_KEY in line:
-                    opinion = ContextsReader.__parse_opinion(line, objects_list=objects_list)
+                if RuAttitudesFormatReader.OPINION_KEY in line:
+                    opinion = RuAttitudesFormatReader.__parse_opinion(line, objects_list=objects_list)
                     opinions_list.append(opinion)
 
-                if ContextsReader.FRAMEVAR_TITLE in line:
+                if RuAttitudesFormatReader.FRAMEVAR_TITLE in line:
                     pass
 
-                if ContextsReader.TERMS_IN_TITLE in line:
-                    title_terms_count = ContextsReader.__parse_terms_in_title_count(line)
+                if RuAttitudesFormatReader.TERMS_IN_TITLE in line:
+                    title_terms_count = RuAttitudesFormatReader.__parse_terms_in_title_count(line)
 
-                if ContextsReader.TERMS_IN_TEXT in line:
-                    text_terms_count = ContextsReader.__parse_terms_in_text_count(line)
+                if RuAttitudesFormatReader.TERMS_IN_TEXT in line:
+                    text_terms_count = RuAttitudesFormatReader.__parse_terms_in_text_count(line)
 
-                if ContextsReader.SINDEX_KEY in line:
-                    s_index = ContextsReader.__parse_sentence_index(line)
+                if RuAttitudesFormatReader.SINDEX_KEY in line:
+                    s_index = RuAttitudesFormatReader.__parse_sentence_index(line)
 
-                if ContextsReader.TEXT_IND_KEY in line:
-                    news_index = ContextsReader.__parse_text_index(line)
+                if RuAttitudesFormatReader.TEXT_IND_KEY in line:
+                    news_index = RuAttitudesFormatReader.__parse_text_index(line)
 
-                if ContextsReader.TITLE_KEY in line:
+                if RuAttitudesFormatReader.TITLE_KEY in line:
                     title = ProcessedSentence(is_title=True,
-                                              parsed_text=ContextsReader.__parse_sentence(line,
-                                                                                          is_title=True,
-                                                                                          stemmer=stemmer),
+                                              parsed_text=RuAttitudesFormatReader.__parse_sentence(line,
+                                                                                                   is_title=True,
+                                                                                                   stemmer=stemmer),
                                               ref_opinions=opinions_list,
                                               objects_list=objects_list,
                                               sentence_index=-1)
@@ -83,11 +82,11 @@ class ContextsReader(object):
                     assert(title_terms_count == len(title.ParsedText) or title_terms_count is None)
                     reset = True
 
-                if ContextsReader.STEXT_KEY in line:
+                if RuAttitudesFormatReader.STEXT_KEY in line:
                     sentence = ProcessedSentence(is_title=False,
-                                                 parsed_text=ContextsReader.__parse_sentence(line,
-                                                                                             is_title=False,
-                                                                                             stemmer=stemmer),
+                                                 parsed_text=RuAttitudesFormatReader.__parse_sentence(line,
+                                                                                                      is_title=False,
+                                                                                                      stemmer=stemmer),
                                                  ref_opinions=opinions_list,
                                                  objects_list=objects_list,
                                                  sentence_index=s_index)
@@ -95,7 +94,7 @@ class ContextsReader(object):
                     assert(text_terms_count == len(sentence.ParsedText) or text_terms_count is None)
                     reset = True
 
-                if ContextsReader.NEWS_SEP_KEY in line and title is not None:
+                if RuAttitudesFormatReader.NEWS_SEP_KEY in line and title is not None:
                     yield ProcessedNews(processed_sentences=processed_sentences,
                                         news_index=news_index)
                     processed_sentences = []
@@ -118,7 +117,7 @@ class ContextsReader(object):
     def __parse_opinion(line, objects_list):
         assert(isinstance(objects_list, list))
 
-        line = line[len(ContextsReader.OPINION_KEY):]
+        line = line[len(RuAttitudesFormatReader.OPINION_KEY):]
 
         s_from = line.index(u'b:(')
         s_to = line.index(u')', s_from)
@@ -147,7 +146,7 @@ class ContextsReader(object):
     @staticmethod
     def __parse_object(line):
         assert(isinstance(line, unicode))
-        line = line[len(ContextsReader.OBJ_KEY):]
+        line = line[len(RuAttitudesFormatReader.OBJ_KEY):]
 
         o_begin = line.index(u"'", 0)
         o_end = line.index(u"'", o_begin + 1)
@@ -172,27 +171,27 @@ class ContextsReader(object):
     def __parse_sentence(line, is_title, stemmer):
         assert(isinstance(is_title, bool))
         assert(isinstance(stemmer, Stemmer) or stemmer is None)
-        key = ContextsReader.STEXT_KEY if not is_title else ContextsReader.TITLE_KEY
+        key = RuAttitudesFormatReader.STEXT_KEY if not is_title else RuAttitudesFormatReader.TITLE_KEY
         text = line[len(key):]
         text = text.strip()
         return TextParser.from_string(str=text, stemmer=stemmer)
 
     @staticmethod
     def __parse_terms_in_title_count(line):
-        line = line[len(ContextsReader.TERMS_IN_TITLE):]
+        line = line[len(RuAttitudesFormatReader.TERMS_IN_TITLE):]
         return int(line)
 
     @staticmethod
     def __parse_terms_in_text_count(line):
-        line = line[len(ContextsReader.TERMS_IN_TEXT):]
+        line = line[len(RuAttitudesFormatReader.TERMS_IN_TEXT):]
         return int(line)
 
     @staticmethod
     def __parse_sentence_index(line):
-        line = line[len(ContextsReader.SINDEX_KEY):]
+        line = line[len(RuAttitudesFormatReader.SINDEX_KEY):]
         return int(line)
 
     @staticmethod
     def __parse_text_index(line):
-        line = line[len(ContextsReader.TEXT_IND_KEY):]
+        line = line[len(RuAttitudesFormatReader.TEXT_IND_KEY):]
         return int(line)
