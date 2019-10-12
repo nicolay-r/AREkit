541f9b3b1f65b366513626f754d524ef063768b4
Nicolay Rusnachenko <kolyarus@yandex.ru>
2019-10-12 12:15:19 +0300

    Refactoring IAN implemnentation into simplified one.

--- networks/context/architectures/ian.py
+++ networks/context/architectures/ian.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tensorflow.python.ops import math_ops
 
 from core.networks.context.architectures.base import BaseContextNeuralNetwork
 from core.networks.context.architectures.sequence import get_cell
@@ -94,7 +93,9 @@ class IAN(BaseContextNeuralNetwork):
             }
 
     def init_embedded_input(self):
-        aspect_inputs = tf.cast(tf.nn.embedding_lookup(self.__E_aspects, self.__aspects), tf.float32)
+        aspect_inputs = tf.cast(x=tf.nn.embedding_lookup(params=self.__E_aspects,
+                                                         ids=self.__aspects),
+                                dtype=tf.float32)
         self.__aspect_inputs = self.optional_process_embedded_data(
             self.Config,
             aspect_inputs,
@@ -103,109 +104,55 @@ class IAN(BaseContextNeuralNetwork):
         return super(IAN, self).init_embedded_input()
 
     def init_context_embedding(self, embedded_terms):
+
         with tf.name_scope('inputs'):
             context_inputs = embedded_terms
             aspect_inputs = self.__aspect_inputs
 
         with tf.name_scope('dynamic_rnn'):
+
+            # Prepare cells
+            aspect_cell = get_cell(hidden_size=self.Config.HiddenSize,
+                                   cell_type=self.Config.CellType,
+                                   dropout_rnn_keep_prob=self.Config.DropoutRNNKeepProb)
+
+            context_cell = get_cell(hidden_size=self.Config.HiddenSize,
+                                    cell_type=self.Config.CellType,
+                                    dropout_rnn_keep_prob=self.Config.DropoutRNNKeepProb)
+
+            # Calculate input lengths
             aspect_lens = utils.calculate_sequence_length(self.__aspects)
-
-            aspect_outputs, aspect_state = tf.nn.dynamic_rnn(
-                cell=get_cell(hidden_size=self.Config.HiddenSize,
-                              cell_type=self.Config.CellType,
-                              dropout_rnn_keep_prob=self.Config.DropoutRNNKeepProb),
-                inputs=aspect_inputs,
-                sequence_length=aspect_lens,
-                dtype=tf.float32,
-                scope='aspect_lstm'
-            )
-
-            aspect_avg = tf.reduce_mean(aspect_outputs, 1)
-
             context_lens = utils.calculate_sequence_length(self.get_input_parameter(InputSample.I_X_INDS))
 
-            context_outputs, context_state = tf.nn.dynamic_rnn(
-                cell=get_cell(hidden_size=self.Config.HiddenSize,
-                              cell_type=self.Config.CellType,
-                              dropout_rnn_keep_prob=self.Config.DropoutRNNKeepProb),
-                inputs=context_inputs,
-                sequence_length=context_lens,
-                dtype=tf.float32,
-                scope='context_lstm'
-            )
+            # Receive aspect output
+            aspect_outputs, _ = tf.nn.dynamic_rnn(cell=aspect_cell,
+                                                  inputs=aspect_inputs,
+                                                  sequence_length=aspect_lens,
+                                                  dtype=tf.float32,
+                                                  scope='aspect_outputs')
+            aspect_avg = tf.reduce_mean(aspect_outputs, 1)
+
+            # Receive context output
+            context_outputs, _ = tf.nn.dynamic_rnn(cell=context_cell,
+                                                   inputs=context_inputs,
+                                                   sequence_length=context_lens,
+                                                   dtype=tf.float32,
+                                                   scope='context_outputs')
             context_avg = tf.reduce_mean(context_outputs, 1)
 
-            aspect_outputs_iter = tf.TensorArray(tf.float32, 1, dynamic_size=True, infer_shape=False)
-            aspect_outputs_iter = aspect_outputs_iter.unstack(aspect_outputs)
-            context_avg_iter = tf.TensorArray(tf.float32, 1, dynamic_size=True, infer_shape=False)
-            context_avg_iter = context_avg_iter.unstack(context_avg)
-            aspect_lens_iter = tf.TensorArray(tf.int32, 1, dynamic_size=True, infer_shape=False)
-            aspect_lens_iter = aspect_lens_iter.unstack(aspect_lens)
-            aspect_rep = tf.TensorArray(size=self.Config.BatchSize, dtype=tf.float32)
-            aspect_att = tf.TensorArray(size=self.Config.BatchSize, dtype=tf.float32)
+            # Attention for aspects
+            aspect_att = tf.nn.softmax(
+                tf.nn.tanh(tf.einsum('ijk,kl,ilm->ijm', aspect_outputs, self.__weights['aspect_score'],
+                                     tf.expand_dims(context_avg, -1)) + self.__biases['aspect_score']))
+            aspect_rep = tf.reduce_sum(aspect_att * aspect_outputs, 1)
 
-            def aspect_body(i, aspect_rep, aspect_att, weights, biases):
-                a = aspect_outputs_iter.read(i)
-                b = context_avg_iter.read(i)
-                l = math_ops.to_int32(aspect_lens_iter.read(i))
-                aspect_score = tf.reshape(tf.nn.tanh(
-                    tf.matmul(tf.matmul(a,
-                                        weights['aspect_score']),
-                                        tf.reshape(b, [-1, 1])) + biases['aspect_score']),
-                                        [1, -1])
-                aspect_att_temp = tf.concat(
-                    [tf.nn.softmax(tf.slice(aspect_score, [0, 0], [1, l])),
-                     tf.zeros([1, self.Config.MaxAspectLength - l])],
-                     1)
-                aspect_att = aspect_att.write(i, aspect_att_temp)
-                aspect_rep = aspect_rep.write(i, tf.matmul(aspect_att_temp, a))
-                return (i + 1, aspect_rep, aspect_att, weights, biases)
+            # Attention for context
+            context_att = tf.nn.softmax(
+                tf.nn.tanh(tf.einsum('ijk,kl,ilm->ijm', context_outputs, self.__weights['context_score'],
+                                     tf.expand_dims(aspect_avg, -1)) + self.__biases['context_score']))
+            context_rep = tf.reduce_sum(context_att * context_outputs, 1)
 
-            def condition(i, aspect_rep, aspect_att, weights, biases):
-                return i < self.Config.BatchSize
-
-            _, aspect_rep_final, aspect_att_final, _, _ = tf.while_loop(
-                cond=condition,
-                body=aspect_body,
-                loop_vars=(0, aspect_rep, aspect_att, self.__weights, self.__biases))
-
-            self.__aspect_atts = tf.reshape(aspect_att_final.stack(), [-1, self.Config.MaxAspectLength])
-            self.__aspect_reps = tf.reshape(aspect_rep_final.stack(), [-1, self.Config.HiddenSize])
-
-            context_outputs_iter = tf.TensorArray(tf.float32, 1, dynamic_size=True, infer_shape=False)
-            context_outputs_iter = context_outputs_iter.unstack(context_outputs)
-            aspect_avg_iter = tf.TensorArray(tf.float32, 1, dynamic_size=True, infer_shape=False)
-            aspect_avg_iter = aspect_avg_iter.unstack(aspect_avg)
-            context_lens_iter = tf.TensorArray(tf.int32, 1, dynamic_size=True, infer_shape=False)
-            context_lens_iter = context_lens_iter.unstack(context_lens)
-            context_rep = tf.TensorArray(size=self.Config.BatchSize, dtype=tf.float32)
-            context_att = tf.TensorArray(size=self.Config.BatchSize, dtype=tf.float32)
-
-            def context_body(i, context_rep, context_att, weights, biases):
-                a = context_outputs_iter.read(i)
-                b = aspect_avg_iter.read(i)
-                l = math_ops.to_int32(context_lens_iter.read(i))
-                context_score = tf.reshape(tf.nn.tanh(
-                    tf.matmul(tf.matmul(a, self.__weights['context_score']), tf.reshape(b, [-1, 1])) + self.__biases['context_score']), [1, -1])
-                context_att_temp = tf.concat(
-                    [tf.nn.softmax(tf.slice(context_score, [0, 0], [1, l])), tf.zeros([1, self.Config.MaxContextLength - l])],
-                    1)
-                context_att = context_att.write(i, context_att_temp)
-                context_rep = context_rep.write(i, tf.matmul(context_att_temp, a))
-                return (i + 1, context_rep, context_att, weights, biases)
-
-            def condition(i, context_rep, context_att, weights, biases):
-                return i < self.Config.BatchSize
-
-            _, context_rep_final, context_att_final, _, _ = tf.while_loop(
-                cond=condition,
-                body=context_body,
-                loop_vars=(0, context_rep, context_att, self.__weights, self.__biases))
-
-            self.__context_atts = tf.reshape(context_att_final.stack(), [-1, self.Config.MaxContextLength])
-            self.__context_reps = tf.reshape(context_rep_final.stack(), [-1, self.Config.HiddenSize])
-
-            return tf.concat([self.__aspect_reps, self.__context_reps], 1)
+            return tf.concat([aspect_rep, context_rep], 1)
 
     def init_logits_unscaled(self, context_embedding):
         return utils.get_k_layer_pair_logits(g=context_embedding,
