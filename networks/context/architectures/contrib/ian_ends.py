0254499d6225a2dcbcef84648f93c24566fc4f41
Nicolay Rusnachenko <kolyarus@yandex.ru>
2019-10-26 14:30:06 +0300

    Added contrib implementation

--- networks/context/architectures/contrib/ian_ends.py
+++ networks/context/architectures/contrib/ian_ends.py
@@ -1,12 +1,34 @@
+import numpy as np
+import tensorflow as tf
 from core.networks.context.architectures.ian import IAN
+from core.networks.context.sample import InputSample
 
 
 class IANAttituteEndsBased(IAN):
     """
-    But original could be based on atitute ends.
+    But original could be based on attitute ends.
     """
+
+    I_ENDS = u'ends'
 
     def __init__(self):
         super(IANAttituteEndsBased, self).__init__()
 
-    # TODO. Complete.
+    @property
+    def AspectInput(self):
+        return self.__ends
+
+    def init_input(self):
+        super(IANAttituteEndsBased, self).init_input()
+        self.__ends = tf.placeholder(dtype=tf.int32,
+                                     shape=[self.__cfg.BatchSize, 2],
+                                     name=u'ctx' + InputSample.I_SUBJ_IND)
+
+    def create_feed_dict(self, input, data_type):
+        feed_dict = super(IANAttituteEndsBased, self).create_feed_dict(input=input,
+                                                                       data_type=data_type)
+
+        feed_dict[self.__ends] = np.concatenate((input[InputSample.I_OBJ_IND],
+                                                 input[InputSample.I_SUBJ_IND]),
+                                                axis=1)
+
---
+++ networks/context/configurations/contrib/att_pcnn.py
@@ -0,0 +1,39 @@
+from core.networks.attention.architectures.mlp import MultiLayerPerceptronAttention
+from core.networks.attention.configurations.mlp import MultiLayerPerceptronAttentionConfig
+from core.networks.context.configurations.cnn import CNNConfig
+
+
+class AttentionPCNNConfig(CNNConfig):
+
+    def __init__(self):
+        super(AttentionPCNNConfig, self).__init__()
+        self.__attention = None
+        self.__attention_config = MultiLayerPerceptronAttentionConfig()
+
+    # region properties
+
+    @property
+    def AttentionModel(self):
+        return self.__attention
+
+    # endregion
+
+    # region public methods
+
+    def notify_initialization_completed(self):
+        assert(self.__attention is None)
+
+        self.__attention = MultiLayerPerceptronAttention(
+            cfg=self.__attention_config,
+            batch_size=self.BatchSize,
+            terms_per_context=self.TermsPerContext,
+            term_embedding_size=self.TermEmbeddingShape[1],
+            pos_embedding_size=self.PosEmbeddingSize,
+            dist_embedding_size=self.DistanceEmbeddingSize)
+
+    def _internal_get_parameters(self):
+        parameters = super(AttentionPCNNConfig, self)._internal_get_parameters()
+        parameters += self.__attention_config.get_parameters()
+        return parameters
+
+    # endregion
