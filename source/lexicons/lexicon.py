00e718d25a4c38d7ed7eff409e0a5eb9b2e9b1af
Nicolay Rusnachenko <kolyarus@yandex.ru>
2019-12-27 18:59:29 +0300

    Implemented reader for rusentilex

--- source/lexicons/lexicon.py
+++ source/lexicons/lexicon.py
@@ -3,8 +3,16 @@ import pandas as pd
 
 class Lexicon(object):
 
+    @property
+    def ToneKey(self):
+        return 'tone'
+
+    @property
+    def TermKey(self):
+        return 'term'
+
     def __init__(self, dataframe):
-        self.__lexicon = dataframe
+        self.__lexicon_df = dataframe
 
     @classmethod
     def from_csv(cls, filepath, separator=','):
@@ -13,12 +21,21 @@ class Lexicon(object):
 
     def get_score(self, lemma):
         assert(type(lemma) == unicode)
-        s = self.__lexicon[lemma.encode('utf-8') == self.__lexicon['term']]
-        return s['tone'].values[0] if len(s) > 0 else 0
+        s = self.__lexicon_df[lemma.encode('utf-8') == self.__lexicon_df[self.TermKey]]
+        return s[self.ToneKey].values[0] if len(s) > 0 else 0
 
     def has_term(self, term):
         assert(type(term) == unicode)
-        s = self.__lexicon[term.encode('utf-8') == self.__lexicon['term']]
+        s = self.__lexicon_df[term.encode('utf-8') == self.__lexicon_df[self.TermKey]]
         return len(s) > 0
+
+    def __iter__(self):
+        for term in self.__lexicon_df[self.TermKey]:
+            yield term
+
+    def __contains__(self, item):
+        assert(isinstance(item, unicode))
+        result = self.__lexicon_df[self.__lexicon_df[self.TermKey] == item.encode('utf-8')]
+        return len(result) > 0
 
 
--- source/lexicons/rusentilex.py
+++ source/lexicons/rusentilex.py
@@ -1,2 +1,38 @@
-# TODO.
-# placeholder now.
+import zipfile
+from os import path
+from os.path import dirname
+
+import pandas as pd
+
+from arekit.source.lexicons.lexicon import Lexicon
+
+
+class RuSentiLexLexicon(Lexicon):
+    """
+    RuSentiLex Lexicon wrapper for csv file stored in /data folder.
+    """
+
+    __INNER_PATH = 'rusentilex.csv'
+
+    @property
+    def ToneKey(self):
+        return 'tone'
+
+    @property
+    def TermKey(self):
+        return 'term'
+
+    @staticmethod
+    def __get_data_root():
+        return path.join(dirname(__file__), u"../../data/")
+
+    @staticmethod
+    def __get_archive_filepath():
+        return path.join(RuSentiLexLexicon.__get_data_root(), u"rusentilex.zip")
+
+    @classmethod
+    def from_zip(cls):
+        with zipfile.ZipFile(cls.__get_archive_filepath(), "r") as zip_ref:
+            with zip_ref.open(cls.__INNER_PATH, mode='r') as csv_file:
+                df = pd.read_csv(csv_file, sep=',')
+                return cls(df)
