b2ac0654c34e69171de172593fc84170449d11f4
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-04-09 11:09:54 +0300

    Fixed bugs. Removed bert as implementation remains incomplete in this release

--- contrib/bert/eval_helper.py
+++
@@ -1,7 +0,0 @@
-class OpinionBasedEvaluationHelper(object):
-
-    def __init__(self, evaluator):
-        self.__evaluator = evaluator
-
-    def evaluate_model(self, data_type, io, doc_ids, epoch_index):
-        pass
