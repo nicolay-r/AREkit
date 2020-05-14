eb28de18200ea3b0eea1e475027e5f87984118b3
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-05-14 22:57:48 +0300

    Refactoring formatters

--- contrib/bert/formatters/opinions/base.py
+++ contrib/bert/formatters/opinions/base.py
@@ -10,15 +10,14 @@ from arekit.common.text_opinions.end_type import EntityEndType
 from arekit.common.text_opinions.helper import TextOpinionHelper
 
 from arekit.common.experiment.formats.base import BaseExperiment
+from arekit.contrib.bert.formatters.base import BaseBertRowsFormatter
 from arekit.contrib.bert.formatters.opinions.provider import OpinionProvider
 from arekit.contrib.bert.formatters.row_ids.multiple import MultipleIDFormatter
 from arekit.contrib.bert.formatters.utils import get_output_dir, generate_filename
 
 
-# TODO. Inherit from base.
-class BertOpinionsFormatter(object):
+class BertOpinionsFormatter(BaseBertRowsFormatter):
 
-    ROW_ID = 'row_id'
     ID = 'id'
     SOURCE = 'source'
     TARGET = 'target'
@@ -26,20 +25,18 @@ class BertOpinionsFormatter(object):
     # region methods
 
     def __init__(self, data_type):
-        assert(isinstance(data_type, unicode))
-        self.__data_type = data_type
-        self.__df = BertOpinionsFormatter.__create_empty_df()
+        super(BertOpinionsFormatter, self).__init__(data_type=data_type)
 
     @staticmethod
-    def __create_empty_df():
-        dtypes_list = []
-        dtypes_list.append((BertOpinionsFormatter.ROW_ID, 'int32'))
+    def formatter_type_log_name():
+        return "Opinion"
+
+    def _get_columns_list_with_types(self):
+        dtypes_list = super(BertOpinionsFormatter, self)._get_columns_list_with_types()
         dtypes_list.append((BertOpinionsFormatter.ID, unicode))
         dtypes_list.append((BertOpinionsFormatter.SOURCE, unicode))
         dtypes_list.append((BertOpinionsFormatter.TARGET, unicode))
-
-        data = np.empty(0, dtype=np.dtype(dtypes_list))
-        return pd.DataFrame(data)
+        return dtypes_list
 
     @staticmethod
     def __create_opinion_row(opinion_provider, linked_wrapper):
@@ -68,9 +65,8 @@ class BertOpinionsFormatter(object):
 
         return row
 
-    # TODO. Make protected
     @staticmethod
-    def __iter_by_rows(opinion_provider):
+    def _iter_by_rows(opinion_provider):
         assert(isinstance(opinion_provider, OpinionProvider))
 
         linked_iter = opinion_provider.iter_linked_opinion_wrappers(balance=False,
@@ -81,22 +77,12 @@ class BertOpinionsFormatter(object):
                 opinion_provider=opinion_provider,
                 linked_wrapper=linked_wrapper)
 
-    # TODO. To Base
-    def __set_value(self, row_ind, column, value):
-        self.__df.at[row_ind, column] = value
-
-    # TODO. To Base
-    def __fill_with_blank_rows(self, rows_count):
-        assert(isinstance(rows_count, int))
-        self.__df[self.ROW_ID] = range(rows_count)
-        self.__df.set_index(self.ROW_ID, inplace=True)
-
     # endregion
 
     def provide_opinion_info_by_opinion_id(self, opinion_id):
         assert(isinstance(opinion_id, unicode))
 
-        opinion_row = self.__df[self.__df[self.ID] == opinion_id]
+        opinion_row = self._df[self._df[self.ID] == opinion_id]
         df_row = opinion_row.iloc[0].tolist()
 
         news_id = df_row[0]
@@ -105,51 +91,32 @@ class BertOpinionsFormatter(object):
 
         return news_id, source, target
 
-    # TODO. To Base
-    # TODO. To Base
-    # TODO. To Base
-    def format(self, opinion_provider):
-        assert(isinstance(opinion_provider, OpinionProvider))
-
-        rows_count = sum(1 for _ in self.__iter_by_rows(opinion_provider))
-
-        self.__fill_with_blank_rows(rows_count)
-        for row_index, row in enumerate(self.__iter_by_rows(opinion_provider)):
-            for column, value in row.iteritems():
-                self.__set_value(row_ind=row_index,
-                                 column=column,
-                                 value=value)
-
-            current_work = row_index + 1
-            total_work = rows_count
-            percent = round(100 * float(current_work) / total_work, 2)
-            print "Opinions ('{}') added: {}/{} ({}%)".format(self.__data_type, current_work, total_work, percent)
-
     def to_tsv_by_experiment(self, experiment):
         assert(isinstance(experiment, BaseExperiment))
 
-        filepath = BertOpinionsFormatter.get_filepath(data_type=self.__data_type,
+        filepath = BertOpinionsFormatter.get_filepath(data_type=self._data_type,
                                                       experiment=experiment)
 
-        self.__df.to_csv(filepath,
-                         sep='\t',
-                         encoding='utf-8',
-                         index=False,
-                         header=False)
+        self._df.to_csv(filepath,
+                        sep='\t',
+                        encoding='utf-8',
+                        columns=[c for c in self._df.columns if c != self.ROW_ID],
+                        index=False,
+                        header=False)
 
     def from_tsv(self, experiment):
         assert(isinstance(experiment, BaseExperiment))
 
-        filepath = BertOpinionsFormatter.get_filepath(data_type=self.__data_type,
+        filepath = BertOpinionsFormatter.get_filepath(data_type=self._data_type,
                                                       experiment=experiment)
 
-        self.__df = pd.read_csv(filepath,
-                                sep='\t',
-                                header=None,
-                                names=[self.ID, self.SOURCE, self.TARGET])
+        self._df = pd.read_csv(filepath,
+                               sep='\t',
+                               header=None,
+                               names=[self.ID, self.SOURCE, self.TARGET])
 
     def get_ids(self):
-        return self.__df.iloc[0].tolist()
+        return self._df.iloc[0].tolist()
 
     @staticmethod
     def get_filepath(data_type, experiment):
--- contrib/bert/formatters/sample/base.py
+++ contrib/bert/formatters/sample/base.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
 from os import path
 
-import numpy as np
 import pandas as pd
 
 import io_utils
@@ -12,6 +11,7 @@ from arekit.common.linked.text_opinions.wrapper import LinkedTextOpinionsWrapper
 from arekit.common.parsed_news.base import ParsedNews
 from arekit.common.text_opinions.text_opinion import TextOpinion
 from arekit.common.experiment.formats.base import BaseExperiment
+from arekit.contrib.bert.formatters.base import BaseBertRowsFormatter
 
 from arekit.contrib.bert.formatters.row_ids.binary import BinaryIDFormatter
 from arekit.contrib.bert.formatters.row_ids.multiple import MultipleIDFormatter
@@ -23,8 +23,7 @@ from arekit.contrib.bert.formatters.sample.text.single import SingleTextProvider
 from arekit.contrib.bert.formatters.utils import get_output_dir, generate_filename
 
 
-# TODO. Inherit from base.
-class BaseSampleFormatter(object):
+class BaseSampleFormatter(BaseBertRowsFormatter):
     """
     Custom Processor with the following fields
 
@@ -35,7 +34,6 @@ class BaseSampleFormatter(object):
     """
     Fields
     """
-    ROW_ID = 'row_id'
     ID = 'id'
     LABEL = 'label'
     S_IND = 's_ind'
@@ -45,12 +43,15 @@ class BaseSampleFormatter(object):
         assert(isinstance(label_provider, BertLabelProvider))
         assert(isinstance(text_provider, SingleTextProvider))
 
-        self.__data_type = data_type
-        # self.__id_col_index = self.__df.column.get_loc[BaseSampleFormatter.ID]
         self.__label_provider = label_provider
         self.__text_provider = text_provider
-        self.__df = self.__create_empty_df()
         self.__row_ids_formatter = self.__create_row_ids_formatter(label_provider)
+
+        super(BaseSampleFormatter, self).__init__(data_type=data_type)
+
+    @staticmethod
+    def formatter_type_log_name():
+        return "Sample"
 
     # region Private methods
 
@@ -62,15 +63,15 @@ class BaseSampleFormatter(object):
             return MultipleIDFormatter()
 
     def __is_train(self):
-        return self.__data_type == DataType.Train
+        return self._data_type == DataType.Train
 
-    def __get_columns_list_with_types(self):
+    def _get_columns_list_with_types(self):
         """
         Composing df with the following columns:
             [id, label, type, text_a]
         """
-        dtypes_list = []
-        dtypes_list.append((self.ROW_ID, 'int32'))
+        dtypes_list = super(BaseSampleFormatter, self)._get_columns_list_with_types()
+
         dtypes_list.append((self.ID, unicode))
 
         # insert labels
@@ -86,10 +87,6 @@ class BaseSampleFormatter(object):
         dtypes_list.append((self.T_IND, 'int32'))
 
         return dtypes_list
-
-    def __create_empty_df(self):
-        data = np.empty(0, dtype=np.dtype(self.__get_columns_list_with_types()))
-        return pd.DataFrame(data)
 
     @staticmethod
     def __get_opinion_end_indices(parsed_news, text_opinion):
@@ -181,18 +178,7 @@ class BaseSampleFormatter(object):
 
         return LinkedTextOpinionsWrapper(linked_text_opinions=linked_opinions)
 
-    # TODO. To base
-    def __fill_with_blank_rows(self, rows_count):
-        assert(isinstance(rows_count, int))
-        self.__df[self.ROW_ID] = range(rows_count)
-        self.__df.set_index(self.ROW_ID, inplace=True)
-
-    # TODO. To base
-    def __set_value(self, row_ind, column, value):
-        self.__df.at[row_ind, column] = value
-
-    # TODO. Make protected
-    def __iter_by_rows(self, opinion_provider):
+    def _iter_by_rows(self, opinion_provider):
         """
         Iterate by rows that is assumes to be added as samples, using opinion_provider information
         """
@@ -216,52 +202,29 @@ class BaseSampleFormatter(object):
 
     # endregion
 
-    # TODO. To Base
-    # TODO. To Base
-    # TODO. To Base
-    def format(self, opinion_provider):
-        """
-        Converts text_opinions into samples by filling related df.
-        """
-        assert(isinstance(opinion_provider, OpinionProvider))
-
-        rows_count = sum(1 for _ in self.__iter_by_rows(opinion_provider))
-        self.__fill_with_blank_rows(rows_count=rows_count)
-
-        for row_index, row in enumerate(self.__iter_by_rows(opinion_provider)):
-            for column, value in row.iteritems():
-                self.__set_value(row_ind=row_index,
-                                 column=column,
-                                 value=value)
-
-            current_work = row_index + 1
-            total_work = rows_count
-            percent = round(100 * float(current_work) / total_work, 2)
-            print "Samples ('{}') added: {}/{} ({}%)".format(self.__data_type, current_work, total_work, percent)
-
     def to_tsv_by_experiment(self, experiment):
         assert(isinstance(experiment, BaseExperiment))
 
-        filepath = self.get_filepath(data_type=self.__data_type,
+        filepath = self.get_filepath(data_type=self._data_type,
                                      experiment=experiment)
 
-        self.__df.to_csv(filepath,
+        self._df.to_csv(filepath,
                          sep='\t',
                          encoding='utf-8',
-                         columns=[c for c in self.__df.columns if c != self.ROW_ID],
+                         columns=[c for c in self._df.columns if c != self.ROW_ID],
                          index=False,
                          float_format="%.0f",
                          header=not self.__is_train())
 
     def from_tsv(self, experiment):
 
-        filepath = self.get_filepath(data_type=self.__data_type,
+        filepath = self.get_filepath(data_type=self._data_type,
                                      experiment=experiment)
 
-        self.__df = pd.read_csv(filepath, sep='\t')
+        self._df = pd.read_csv(filepath, sep='\t')
 
     def extract_ids(self):
-        return self.__df[self.ID].astype(unicode).tolist()
+        return self._df[self.ID].astype(unicode).tolist()
 
     @staticmethod
     def extract_row_id(opinion_row):
@@ -283,4 +246,4 @@ class BaseSampleFormatter(object):
         return filepath
 
     def __len__(self):
-        return len(self.__df.index)
+        return len(self._df.index)
