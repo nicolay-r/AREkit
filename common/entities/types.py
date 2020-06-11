3fbbae8e392a1b0f7d7437b09be179058ffa8bb0
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-06-11 14:13:07 +0300

    Fixed bug: incorrect enumeration.

--- common/entities/types.py
+++ common/entities/types.py
@@ -4,6 +4,6 @@ from enum import Enum
 class EntityType(Enum):
     Object = 1
     Subject = 2
-    SynonymSubject = 2
-    SynonymObject = 2
-    Other = 3
+    SynonymSubject = 3
+    SynonymObject = 4
+    Other = 5
