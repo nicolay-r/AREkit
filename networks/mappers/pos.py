4b2d4d9ac1e8624638eae5d9e830c440b5e2e755
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-06-12 13:23:21 +0300

    Fixed references

--- networks/mappers/pos.py
+++ networks/mappers/pos.py
@@ -1,5 +1,6 @@
 import collections
 
+from arekit.common.languages.pos import PartOfSpeechType
 from arekit.processing.pos.base import POSTagger
 from arekit.processing.text.token import Token
 
@@ -11,10 +12,10 @@ def iter_pos_indices_for_terms(terms, pos_tagger):
 
     for index, term in enumerate(terms):
         if isinstance(term, Token):
-            pos = pos_tagger.Empty
+            pos = PartOfSpeechType.Empty
         elif isinstance(term, unicode):
             pos = pos_tagger.get_term_pos(term)
         else:
-            pos = pos_tagger.Unknown
+            pos = PartOfSpeechType.Unknown
 
         yield pos_tagger.pos_to_int(pos)
