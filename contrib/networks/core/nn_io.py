e9ace50569a70edca2c3f4fbc09e5eb892d0fd72
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-10-29 21:46:10 +0300

    switched to full_model_name terminology

--- contrib/networks/core/nn_io.py
+++ contrib/networks/core/nn_io.py
@@ -7,15 +7,15 @@ class NeuralNetworkModelIO(BaseModelIO):
     """ Provides an API for saving model states
     """
 
-    def __init__(self, model_dir, model_name):
+    def __init__(self, model_dir, full_model_name):
         self.__model_dir = model_dir
-        self.__model_name = model_name
+        self.__full_model_name = full_model_name
 
     def get_model_dir(self):
-        return join(self.__model_dir, self.__model_name)
+        return join(self.__model_dir, self.__full_model_name)
 
     def get_model_save_path_tf_prefix(self):
         """ Provides the template for states keeping
             using tensorflow serializer.
         """
-        return join(self.__model_dir, u'{}'.format(self.__model_name))
+        return join(self.__model_dir, self.__full_model_name)
