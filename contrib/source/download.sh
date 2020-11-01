226a151037d05bd522ac750a4c004883c0bc5676
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-11-01 17:19:26 +0300

    Providing reference and download link for RuAttitudes-v1.2

--- contrib/source/download.sh
+++ contrib/source/download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 data_dir="./data/"
+# Downloading RuAttitudes-1.2
+curl -L -o "$data_dir/ruattitudes-v1_2.zip" https://github.com/nicolay-r/RuAttitudes/raw/v1.2/ruattitudes_v1_2.zip
 # Downloading RuAttitudes-2.0-base
 curl -L -o "$data_dir/ruattitudes-v2_0_base.zip" https://www.dropbox.com/s/y39vqzzjumqhce1/ruattitudes_20_base.zip?dl=1
 # Downloading RuAttitudes-2.0-large
