35ae6347d5c13fc343b18f3a5d7766e54c1bbe4e
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-11-08 13:31:36 +0300

    Providing search by type

--- contrib/bert/entity/types.py
+++ contrib/bert/entity/types.py
@@ -17,9 +17,22 @@ class BertEntityFormattersService:
     }
 
     @staticmethod
+    def __iter_supported_names():
+        return iter(BertEntityFormattersService.__names.keys())
+
+    @staticmethod
     def get_type_by_name(name):
         return BertEntityFormattersService.__names[name]
 
     @staticmethod
+    def find_name_by_type(entity_fmt_type):
+        assert(isinstance(entity_fmt_type, BertEntityFormatterTypes))
+
+        for name in BertEntityFormattersService.__iter_supported_names():
+            related_type = BertEntityFormattersService.__names[name]
+            if related_type == entity_fmt_type:
+                return name
+
+    @staticmethod
     def iter_supported_names():
-        return iter(BertEntityFormattersService.__names.keys())
+        return BertEntityFormattersService.__iter_supported_names()
