b2a72832f9bede4bc2b810c70cf6366d32ee7cdc
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-11-08 18:47:35 +0300

    refering to const variable

--- contrib/bert/core/result/binary.py
+++ contrib/bert/core/result/binary.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+from arekit.common.experiment import const
 from arekit.common.experiment.output.base import BaseOutput
 from arekit.common.experiment.input.formatters.opinion import BaseOpinionsFormatter
 from arekit.common.experiment.input.providers.row_ids.base import BaseIDProvider
@@ -34,7 +35,7 @@ class BertBinaryOutput(BaseOutput):
         paper: https://www.aclweb.org/anthology/N19-1035.pdf
         """
         ind_max = df[BertBinaryOutput.YES].idxmax()
-        sample_id = df.loc[ind_max][self.ID]
+        sample_id = df.loc[ind_max][const.ID]
         uint_label = self._IdsFormatter.parse_label_in_sample_id(sample_id)
         return self.__labels_scaler.uint_to_label(value=uint_label)
 
@@ -47,16 +48,16 @@ class BertBinaryOutput(BaseOutput):
         for opinion_ind in self.__iter_linked_opinion_indices(linked_df=linked_df):
             ind_pattern = self._IdsFormatter.create_pattern(id_value=opinion_ind,
                                                             p_type=BaseIDProvider.INDEX)
-            opinion_df = linked_df[linked_df[self.ID].str.contains(ind_pattern)]
+            opinion_df = linked_df[linked_df[const.ID].str.contains(ind_pattern)]
 
             yield self._compose_opinion_by_opinion_id(
-                sample_id=opinion_df[self.ID].iloc[0],
+                sample_id=opinion_df[const.ID].iloc[0],
                 # TODO. Replace with opinions reader.
                 opinions_reader=opinions_reader,
                 calc_label_func=lambda: self.__calculate_label(df=opinion_df))
 
     def __iter_linked_opinion_indices(self, linked_df):
-        sample_ids = linked_df[self.ID].tolist()
+        sample_ids = linked_df[const.ID].tolist()
         all_news = [self._IdsFormatter.parse_index_in_sample_id(sample_id)
                     for sample_id in sample_ids]
         for news_id in set(all_news):
