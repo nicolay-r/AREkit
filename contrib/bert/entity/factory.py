8a2c54aeaa2d803e635ffcc1fde21b9a425aa098
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-11-10 10:18:53 +0300

    fixed parameter type check

--- contrib/bert/entity/factory.py
+++ contrib/bert/entity/factory.py
@@ -8,7 +8,7 @@ def create_bert_entity_formatter(fmt_type, create_russian_pos_tagger_func=None):
     """ Factory method for entity formatters, applicable in bert.
     """
     assert(isinstance(fmt_type, BertEntityFormatterTypes))
-    assert(callable(create_russian_pos_tagger_func))
+    assert(callable(create_russian_pos_tagger_func) or create_russian_pos_tagger_func is None)
 
     if fmt_type == BertEntityFormatterTypes.RussianCased:
         return RussianEntitiesCasedFormatter(create_russian_pos_tagger_func())
