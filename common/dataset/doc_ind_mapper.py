6fa4e3a418d779cfae59d168150779371e6b9e8d
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-11-17 11:56:13 +0300

    Removed non used files

--- common/dataset/doc_ind_mapper.py
+++
@@ -1,9 +0,0 @@
-
-class DocumentIndexMapper(object):
-    """
-    Provides str to int index mapping in order to support a multiple collections
-    aggregation in dataset.
-    """
-
-    def __init__(self):
-        pass
