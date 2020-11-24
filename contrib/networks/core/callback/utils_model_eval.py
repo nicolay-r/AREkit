b495198c0c4e5b5451cb778ddf0db31c833590e6
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-11-24 13:54:54 +0300

    Callback refactoring. All the related to output information has been moved out of AREkit into the networks repo.

--- contrib/networks/core/callback/utils_model_eval.py
+++ contrib/networks/core/callback/utils_model_eval.py
@@ -7,7 +7,57 @@ from arekit.common.experiment.output.opinions.converter import OutputToOpinionCo
 from arekit.common.experiment.output.opinions.writer import save_opinion_collections
 from arekit.common.labels.str_fmt import StringLabelsFormatter
 from arekit.common.model.labeling.modes import LabelCalculationMode
+from arekit.contrib.networks.core.callback.utils_hidden_states import save_minibatch_all_input_dependent_hidden_values
+from arekit.contrib.networks.core.data_handling.predict_log import NetworkInputDependentVariables
 from arekit.contrib.networks.core.input.readers.samples import NetworkInputSampleReader
+from arekit.contrib.networks.core.model import BaseTensorflowModel
+from arekit.contrib.networks.core.output.encoder import NetworkOutputEncoder
+from arekit.contrib.source.rusentrel.labels_fmt import RuSentRelLabelsFormatter
+
+
+def evaluate_model(experiment, data_type, epoch_index, model,
+                   labels_formatter, save_hidden_params, log_dir):
+    """ Performs Model Evaluation on a particular state (i.e. epoch),
+        for a particular data type.
+    """
+    assert(isinstance(labels_formatter, RuSentRelLabelsFormatter))
+    assert(isinstance(model, BaseTensorflowModel))
+    assert(isinstance(data_type, DataType))
+    assert(isinstance(epoch_index, int))
+    assert(isinstance(save_hidden_params, bool))
+
+    # Prediction result is a pair of the following parameters:
+    # idhp -- input dependent variables that might be saved for additional research.
+    # output -- output encoder of the network.
+    idhp, output = model.predict(data_type=data_type)
+
+    assert (isinstance(idhp, NetworkInputDependentVariables))
+    assert (isinstance(output, NetworkOutputEncoder))
+
+    # Crate filepath
+    result_filepath = experiment.ExperimentIO.get_output_model_results_filepath(
+        data_type=data_type,
+        epoch_index=epoch_index)
+
+    # Save output
+    output.to_tsv(filepath=result_filepath)
+
+    # Convert output to result.
+    result = perform_experiment_evaluation(
+        experiment=experiment,
+        data_type=data_type,
+        epoch_index=epoch_index,
+        labels_formatter=RuSentRelLabelsFormatter())
+
+    # optionally save input-dependend hidden parameters.
+    if save_hidden_params:
+        save_minibatch_all_input_dependent_hidden_values(
+            predict_log=idhp,
+            data_type=data_type,
+            log_dir=log_dir,
+            epoch_index=epoch_index)
+
+    return result
 
 
 def perform_experiment_evaluation(experiment, data_type, epoch_index, labels_formatter):
