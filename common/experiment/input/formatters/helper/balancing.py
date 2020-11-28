9522ed2e74b0be1fa72bdad608f2b9fc10b8c5a5
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-11-28 14:58:44 +0300

    Providing amount of examples after balancing.

--- common/experiment/input/formatters/helper/balancing.py
+++ common/experiment/input/formatters/helper/balancing.py
@@ -18,12 +18,9 @@ class SampleRowBalancerHelper(object):
         return df[df[const.LABEL] == uint_label]
 
     @staticmethod
-    def __get_largest_class_size(df, uint_labels):
-
-        sizes = [len(SampleRowBalancerHelper.__get_class(df=df, uint_label=uint_label))
-                 for uint_label in uint_labels]
-
-        return max(sizes)
+    def __get_class_sizes(df, uint_labels):
+        return [len(SampleRowBalancerHelper.__get_class(df=df, uint_label=uint_label))
+                for uint_label in uint_labels]
 
     @staticmethod
     def __fill_blank(label_df, blank_df, seed=1):
@@ -56,11 +53,13 @@ class SampleRowBalancerHelper(object):
 
         output_labels = label_provider.OutputLabelsUint
 
-        class_size = SampleRowBalancerHelper.__get_largest_class_size(df=df, uint_labels=output_labels)
+        original_class_sizes = SampleRowBalancerHelper.__get_class_sizes(df=df, uint_labels=output_labels)
+
+        larges_class_size = max(original_class_sizes)
 
         balanced = [SampleRowBalancerHelper.__fill_blank(
                         label_df=SampleRowBalancerHelper.__get_class(df=df, uint_label=label),
-                        blank_df=create_blank_df(class_size))
+                        blank_df=create_blank_df(larges_class_size))
                     for label in output_labels]
 
         balanced_df = pd.concat(balanced)
@@ -68,6 +67,19 @@ class SampleRowBalancerHelper(object):
         balanced_df.sort_values(by=[const.ID],
                                 inplace=True,
                                 ascending=True)
+
+        balanced_class_sizes = SampleRowBalancerHelper.__get_class_sizes(df=balanced_df,
+                                                                         uint_labels=output_labels)
+
+        logger.info(u"Rows count for uint labels [original]: {}".format(
+            ["{} ({})".format(str(class_uint), str(count))
+             for class_uint, count in enumerate(original_class_sizes) if count > 0]))
+
+        logger.info(u"Rows count for uint labels [balanced]: {}".format(
+            ["{} ({})".format(str(class_uint), str(count))
+             for class_uint, count in enumerate(balanced_class_sizes) if count > 0]))
+
+        logger.info(u"Rows count [balanced, total]: {}".format(sum(balanced_class_sizes)))
 
         return balanced_df
 
