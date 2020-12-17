37f3722c8f42e69902421f9aa0d8d2b09f9293bd
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-12-17 22:36:29 +0300

    Refactoring

--- tests/test_labels.py
+++ tests/test_labels.py
@@ -1,12 +1,22 @@
 import sys
+import unittest
 
 sys.path.append('../')
 
 from arekit.common.labels.base import PositiveLabel, NegativeLabel, Label, NeutralLabel
 
-p = PositiveLabel()
-n = NegativeLabel()
-u = NeutralLabel()
-l = Label()
 
-print u == u
+class TestLabels(unittest.TestCase):
+
+    def test(self):
+
+        pos_label = PositiveLabel()
+        neg_label = NegativeLabel()
+        neutral_label = NeutralLabel()
+        label = Label()
+
+        print neutral_label == neutral_label
+
+
+if __name__ == '__main__':
+    unittest.main()
