0a353cc996ea532fbc46e26f7dd88f501c0632cd
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-12-20 17:34:19 +0300

    Related to #48

--- contrib/miner/start_deep_ner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#
-# TODO. This should be removed.
-#
-
-# Download and run NER flask wrapper
-git clone https://github.com/nicolay-r/ner-bilstm-crf-tensorflow
-cd ner-flask-wrapper && nohup python3 start.py &
--- contrib/miner/start_syntaxnet.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run SyntaxNet docker
-docker run --shm-size=1024m -ti --rm -p 8111:9999 inemo/syntaxnet_rus server 0.0.0.0 9999
