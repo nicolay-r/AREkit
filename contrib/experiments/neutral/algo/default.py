36760589ee4fe77d4ee2ed47a9470f3414631e5d
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-12-30 13:07:34 +0300

    For WIMS-2020 version (#32) we also comment this feature in order to obtain better results.

--- contrib/experiments/neutral/algo/default.py
+++ contrib/experiments/neutral/algo/default.py
@@ -151,10 +151,10 @@ class DefaultNeutralAnnotationAlgorithm(BaseNeutralAnnotationAlgorithm):
                 if s_dist > 0:
                     continue
 
-                t_dist = self.__get_distance_in_terms_between_entities(n_id=news_id, e1=e1, e2=e2)
+                # t_dist = self.__get_distance_in_terms_between_entities(n_id=news_id, e1=e1, e2=e2)
 
-                if t_dist > 10:
-                    continue
+                # if t_dist > 10:
+                #    continue
 
                 if sentiment_opinions is not None:
                     o = Opinion(source_value=e1.Value,
