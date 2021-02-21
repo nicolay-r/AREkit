8859f32675fcc61420feb68f5be50a40bf81a93e
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-02-21 16:31:10 +0300

    __stol field made protected in order to provide the access from the outside

--- common/labels/str_fmt.py
+++ common/labels/str_fmt.py
@@ -5,16 +5,16 @@ class StringLabelsFormatter(object):
 
     def __init__(self, stol):
         assert(isinstance(stol, dict))
-        self.__stol = stol
+        self._stol = stol
 
     def str_to_label(self, value):
         assert(isinstance(value, unicode))
-        assert(value in self.__stol)
-        return self.__stol[value]
+        assert(value in self._stol)
+        return self._stol[value]
 
     def label_to_str(self, label):
         assert(isinstance(label, Label))
-        for value, l in self.__stol.iteritems():
+        for value, l in self._stol.iteritems():
             if l == label:
                 return value
 
