dcc5b72d609b9abc4066967d60db4beca2ca7386
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-05-04 21:41:34 +0300

    Fixed unittest relate to frames stat printing.

--- contrib/source/tests/test_show_frames_stat.py
+++ contrib/source/tests/test_show_frames_stat.py
@@ -1,9 +1,17 @@
 import sys
+import unittest
 
 sys.path.append('../../../../')
 
+from arekit.contrib.source.run_rusentiframes_stat import about_version
 from arekit.contrib.source.rusentiframes.types import RuSentiFramesVersions
-from arekit.contrib.source.rusentiframes.stat import about_version
 
 
-about_version(version=RuSentiFramesVersions.V20)
+class TestFramesStat(unittest.TestCase):
+
+    def test_stat(self):
+        about_version(version=RuSentiFramesVersions.V20)
+
+
+if __name__ == '__main__':
+    unittest.main()
