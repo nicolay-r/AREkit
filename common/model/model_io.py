351476d891045943f10c2ac1abb8718d5c018a8c
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-05-27 10:51:37 +0300

    Fixed #79

--- common/model/model_io.py
+++ common/model/model_io.py
@@ -5,8 +5,3 @@ class BaseModelIO(object):
 
     def get_model_name(self):
         raise NotImplementedError()
-
-    # TODO. This is a part of the neural network model.
-    # Therefore the latter should be moved in a nested class.
-    def get_model_dir(self):
-        raise NotImplementedError()
