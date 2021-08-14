dc91fd02a2b1a0b6fda88fa532495757bbc53f82
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-08-14 10:45:10 +0300

    Fixed utf-8 encoding bug which leads to unit-test freezing.

--- tests/contrib/source/test_rusentiframes.py
+++ tests/contrib/source/test_rusentiframes.py
@@ -26,23 +26,23 @@ class TestRuSentiFrames(unittest.TestCase):
             # id
             logger.info(u"Frame: {}".format(frame_id))
             # titles
-            logger.info(u"Titles: {}".format(u",".join(frames.get_frame_titles(frame_id))).encode('utf-8'))
+            logger.info(u"Titles: {}".format(u",".join(frames.get_frame_titles(frame_id))))
             # variants
-            logger.info(u"Variants: {}".format(u",".join(frames.get_frame_variants(frame_id))).encode('utf-8'))
+            logger.info(u"Variants: {}".format(u",".join(frames.get_frame_variants(frame_id))))
             # roles
             for role in frames.get_frame_roles(frame_id):
-                logger.info(u"Role: {}".format(u" -- ".join([role.Source, role.Description])).encode('utf-8'))
+                logger.info(u"Role: {}".format(u" -- ".join([role.Source, role.Description])))
             # states
             for state in frames.get_frame_states(frame_id):
-                logger.info(u"State: {}".format(u",".join([state.Role, state.Label.to_class_str(), str(state.Prob)])).encode('utf-8'))
+                logger.info(u"State: {}".format(u",".join([state.Role, state.Label.to_class_str(), str(state.Prob)])))
             # polarity
             for polarity in frames.get_frame_polarities(frame_id):
                 logger.info(u"Polarity: {}".format(u",".join([polarity.Source,
                                                               polarity.Destination,
-                                                              polarity.Label.to_class_str()])).encode('utf-8'))
+                                                              polarity.Label.to_class_str()])))
 
             has_a0_a1_pol = frames.try_get_frame_polarity(frame_id, role_src=u"a0", role_dest=u"a1")
-            logger.info(u"Has a0->a1 polarity: {}".format(has_a0_a1_pol is not None).encode('utf-8'))
+            logger.info(u"Has a0->a1 polarity: {}".format(has_a0_a1_pol is not None))
 
         # frame variants.
         frame_variants = FrameVariantsCollection()
@@ -52,8 +52,8 @@ class TestRuSentiFrames(unittest.TestCase):
 
         assert(isinstance(frame_variant, FrameVariant))
 
-        logger.info(u"FrameVariantValue: {}".format(frame_variant.get_value()).encode('utf-8'))
-        logger.info(u"FrameID: {}".format(frame_variant.FrameID).encode('utf-8'))
+        logger.info(u"FrameVariantValue: {}".format(frame_variant.get_value()))
+        logger.info(u"FrameID: {}".format(frame_variant.FrameID))
 
 
 if __name__ == '__main__':
