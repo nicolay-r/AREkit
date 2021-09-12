84a8da5c4ec6464387fdccd7a773159799c56c15
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-09-12 12:42:04 +0300

    Removed non utilized methods

--- arekit/common/opinions/provider.py
+++ arekit/common/opinions/provider.py
@@ -5,9 +5,3 @@ class OpinionCollectionsProvider(object):
 
     def serialize(self, collection, filepath, labels_formatter, error_on_non_supported):
         raise NotImplementedError()
-
-    def __enter__(self):
-        pass
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
