65c2e245c84510469dbc20727c3db3b544bd6564
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-09-22 13:28:54 +0300

    Removed non utilized imports

--- arekit/common/experiment/extract/opinions.py
+++ arekit/common/experiment/extract/opinions.py
@@ -1,8 +1,5 @@
 import collections
 
-from arekit.common.experiment.data_type import DataType
-from arekit.common.experiment.formats.documents import DocumentOperations
-from arekit.common.experiment.formats.opinions import OpinionOperations
 from arekit.common.linked.data import LinkedDataWrapper
 from arekit.common.linked.text_opinions.wrapper import LinkedTextOpinionsWrapper
 from arekit.common.model.labeling.base import LabelsHelper
