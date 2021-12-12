162e949a4e358b693594e3096659b0da456c4a6b
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-12-12 13:10:46 +0300

    #161. Added data downloader

---
+++ examples/data.py
@@ -0,0 +1,15 @@
+from os.path import join
+from arekit.contrib.source import utils
+
+
+def download_examples_data():
+    root_dir = utils.get_default_download_dir()
+
+    data = {
+        "rusvectores_news.zip": "http://rusvectores.org/static/models/rusvectores2/news_mystem_skipgram_1000_20_2015.bin.gz",
+    }
+
+    # Perform downloading ...
+    for local_name, url_link in data.items():
+        utils.download(dest_file_path=join(root_dir, local_name),
+                       source_url=url_link)
