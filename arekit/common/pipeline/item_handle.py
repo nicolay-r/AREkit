44cd493ff1b19f5fc2a8cbd9f2492d22242537ad
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-12-30 14:38:17 +0300

    Fixed handle item impelementation; related to #154

--- arekit/common/pipeline/item_handle.py
+++ arekit/common/pipeline/item_handle.py
@@ -10,14 +10,14 @@ class HandleIterPipelineItem(BasePipelineItem):
         assert(callable(handle_func))
         self.__handle_func = handle_func
 
+    def __updated_data(self, items_iter):
+        for item in items_iter:
+            # Perform item handling
+            self.__handle_func(item)
+            yield item
+
     def apply(self, pipeline_ctx):
         assert(isinstance(pipeline_ctx, PipelineContext))
         items_iter = pipeline_ctx.provide("src")
         assert(isinstance(items_iter, collections.Iterable))
-
-        for item in items_iter:
-
-            # Perform item handling
-            self.__handle_func(item)
-
-            yield item
+        pipeline_ctx.update("src", value=self.__updated_data(items_iter))
