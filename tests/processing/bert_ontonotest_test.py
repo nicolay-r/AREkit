38a0be91a0c0c5b49165c4c779e1c308987491ee
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-02-10 20:36:52 +0300

    DeepPavlov NER related test examples refactoring (causes Memory Limit issue, once declared in a single test file)

--- tests/processing/bert_ontonotest_test.py
+++ tests/processing/bert_ontonotest_test.py
@@ -1,12 +1,7 @@
 import unittest
 
-from arekit.common.news.base import News
-from arekit.common.news.parser import NewsParser
-from arekit.common.news.sentence import BaseNewsSentence
-from arekit.common.text.parser import BaseTextParser
 from arekit.processing.entities.bert_ontonotes import BertOntonotesNER
 from arekit.processing.entities.obj_desc import NerObjectDescriptor
-from arekit.processing.text.pipeline_entities_bert_ontonotes import BertOntonotesNERPipelineItem
 
 
 class BertOntonotesTest(unittest.TestCase):
@@ -29,12 +24,6 @@ class BertOntonotesTest(unittest.TestCase):
                 print(s_obj.Position)
                 print(s_obj.Length)
                 print(tokens[s_obj.Position:s_obj.Position + s_obj.Length])
-
-    def test_pipeline(self):
-        text_parser = BaseTextParser([BertOntonotesNERPipelineItem()])
-        news = News(doc_id=0, sentences=[BaseNewsSentence(self.text.split())])
-        parsed_news = NewsParser.parse(news=news, text_parser=text_parser)
-        print(parsed_news.iter_sentence_terms(sentence_index=0))
 
 
 if __name__ == '__main__':
---
+++ tests/processing/ner_pipeline_test.py
@@ -0,0 +1,25 @@
+import unittest
+
+from arekit.common.news.base import News
+from arekit.common.news.parser import NewsParser
+from arekit.common.news.sentence import BaseNewsSentence
+from arekit.common.text.parser import BaseTextParser
+from arekit.processing.text.pipeline_entities_bert_ontonotes import BertOntonotesNERPipelineItem
+
+
+class BertOntonotesPipelineItemTest(unittest.TestCase):
+
+    text = "США пытается ввести санкции против Российской Федерацией"
+
+    def test_pipeline(self):
+        text_parser = BaseTextParser([BertOntonotesNERPipelineItem()])
+        news = News(doc_id=0, sentences=[BaseNewsSentence(self.text.split())])
+        parsed_news = NewsParser.parse(news=news, text_parser=text_parser)
+        terms = parsed_news.iter_sentence_terms(sentence_index=0, return_id=False)
+
+        for term in terms:
+            print(term)
+
+
+if __name__ == '__main__':
+    unittest.main()
