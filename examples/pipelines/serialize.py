884a2812caa058c73c5e3189d34e611194662c6a
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-02-18 16:47:24 +0300

    #161 considering a case of a single sentence as well

--- examples/pipelines/serialize.py
+++ examples/pipelines/serialize.py
@@ -38,11 +38,14 @@ from examples.network.text_parser.entities import TextEntitiesParser
 
 def run_data_serialization_pipeline(sentences, terms_per_context, entities_parser,
                                     embedding_path, entity_fmt_type, stemmer):
-    assert(isinstance(sentences, list))
+    assert(isinstance(sentences, list) or isinstance(sentences, str))
     assert(isinstance(entities_parser, BasePipelineItem) or entities_parser is None)
     assert(isinstance(terms_per_context, int))
     assert(isinstance(embedding_path, str))
     assert(isinstance(entity_fmt_type, EntityFormatterTypes))
+
+    if isinstance(sentences, str):
+        sentences = [sentences]
 
     labels_scaler = BaseLabelScaler(uint_dict=OrderedDict([(NoLabel(), 0)]),
                                     int_dict=OrderedDict([(NoLabel(), 0)]))
