d7542216f37dacbd12ca9fd925a065ccd7db1698
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-06-15 16:28:41 +0300

    Guarantee the correct matching.

--- arekit/common/news/parsed/providers/base.py
+++ arekit/common/news/parsed/providers/base.py
@@ -13,6 +13,9 @@ class BaseParsedNewsServiceProvider(object):
 
     def init_parsed_news(self, parsed_news):
         assert(isinstance(parsed_news, ParsedNews))
-        self._doc_entities = [DocumentEntity(id_in_doc=doc_id, value=entity.Value,
-                                             e_type=entity.Type, group_index=entity.GroupIndex)
-                              for doc_id, entity in enumerate(parsed_news.iter_entities())]
+        self._doc_entities = {}
+        for doc_id, entity in enumerate(parsed_news.iter_entities()):
+            self._doc_entities[doc_id] = DocumentEntity(id_in_doc=doc_id,
+                                                        value=entity.Value,
+                                                        e_type=entity.Type,
+                                                        group_index=entity.GroupIndex)
