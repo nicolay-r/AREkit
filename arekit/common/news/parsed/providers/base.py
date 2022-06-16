f753f93d083fe91cef87f3c8e3250c29380d7a68
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-06-16 17:43:08 +0300

    #337 rollback

--- arekit/common/news/parsed/providers/base.py
+++ arekit/common/news/parsed/providers/base.py
@@ -13,10 +13,6 @@ class BaseParsedNewsServiceProvider(object):
 
     def init_parsed_news(self, parsed_news):
         assert(isinstance(parsed_news, ParsedNews))
-        self._doc_entities = {}
-        for entity in parsed_news.iter_entities():
-            assert(entity.ID not in self._doc_entities)
-            self._doc_entities[entity.ID] = DocumentEntity(id_in_doc=entity.ID,
-                                                           value=entity.Value,
-                                                           e_type=entity.Type,
-                                                           group_index=entity.GroupIndex)
+        self._doc_entities = [DocumentEntity(id_in_doc=doc_id, value=entity.Value,
+                                             e_type=entity.Type, group_index=entity.GroupIndex)
+                              for doc_id, entity in enumerate(parsed_news.iter_entities())]
