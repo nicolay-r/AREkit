593ea3d070235b91a8e01b4360a3f53dc42a279b
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-06-17 09:32:03 +0300

    Provided description

--- arekit/contrib/experiment_rusentrel/connotations/provider.py
+++ arekit/contrib/experiment_rusentrel/connotations/provider.py
@@ -3,6 +3,15 @@ from arekit.contrib.source.rusentiframes.collection import RuSentiFramesCollecti
 
 
 class RuSentiFramesConnotationProvider(FrameConnotationProvider):
+    """ This is a provider based on A0->A1 label type of RuSentiFrames collection.
+        For a greater details, checkout the related collection at:
+        https://github.com/nicolay-r/RuSentiFrames
+
+        Papers:
+            [1] Natalia Loukachevitch, Nicolay Rusnachenko: Sentiment Frames
+                for Attitude Extraction in Russian, 2020
+            [2] Distant Supervision for Sentiment Attitude Extraction, 2019
+    """
 
     def __init__(self, collection):
         assert(isinstance(collection, RuSentiFramesCollection))
