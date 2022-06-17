76adaef3facb6decf76a9fc2197ccffaf2ff96a3
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-06-17 09:55:57 +0300

    #338 update

--- arekit/common/news/entity.py
+++ arekit/common/news/entity.py
@@ -4,6 +4,8 @@ from arekit.common.entities.base import Entity
 class DocumentEntity(Entity):
 
     def __init__(self, value, e_type, id_in_doc, group_index):
+        """ id_in_doc: Id, utilized witin the internal services
+        """
         super(DocumentEntity, self).__init__(value=value,
                                              e_type=e_type,
                                              group_index=group_index)
--- arekit/common/news/parsed/providers/base.py
+++ arekit/common/news/parsed/providers/base.py
@@ -1,11 +1,19 @@
+from arekit.common.entities.base import Entity
 from arekit.common.news.entity import DocumentEntity
 from arekit.common.news.parsed.base import ParsedNews
 
 
 class BaseParsedNewsServiceProvider(object):
 
-    def __init__(self):
+    def __init__(self, entity_index_func):
+        """ Outside enity indexing function
+            entity_index_func: provides id for a given entity, i.e.
+                func(entity) -> int (id)
+        """
+        assert(callable(entity_index_func))
         self._doc_entities = None
+        self.__entity_map = {}
+        self.__entity_index_func = entity_index_func
 
     @property
     def Name(self):
@@ -13,6 +21,25 @@ class BaseParsedNewsServiceProvider(object):
 
     def init_parsed_news(self, parsed_news):
         assert(isinstance(parsed_news, ParsedNews))
-        self._doc_entities = [DocumentEntity(id_in_doc=doc_id, value=entity.Value,
-                                             e_type=entity.Type, group_index=entity.GroupIndex)
-                              for doc_id, entity in enumerate(parsed_news.iter_entities())]
+
+        self._doc_entities = []
+        self.__entity_map.clear()
+
+        for index, entity in enumerate(parsed_news.iter_entities()):
+
+            doc_entity = DocumentEntity(id_in_doc=index,
+                                        value=entity.Value,
+                                        e_type=entity.Type,
+                                        group_index=entity.GroupIndex)
+
+            self._doc_entities.append(doc_entity)
+            self.__entity_map[self.__entity_index_func(entity)] = doc_entity
+
+    def get_document_entity(self, entity):
+        """ Maps entity to the related one with DocumentEntity type
+        """
+        assert(isinstance(entity, Entity))
+        return self.__entity_map[self.__entity_index_func(entity)]
+
+    def contains_entity(self, entity):
+        return self.__entity_index_func(entity) in self.__entity_map
