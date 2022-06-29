e993e821bf9c1c07055a006d72769b1dc08aa7b0
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-06-29 18:45:36 +0300

    Refactoring

--- arekit/contrib/utils/pipelines/annot/opinion_annotation.py
+++ arekit/contrib/utils/pipelines/annot/opinion_annotation.py
@@ -1,13 +1,11 @@
 from arekit.common.data.input.sample import InputSampleBase
-from arekit.common.linkage.text_opinions import TextOpinionsLinkage
 from arekit.common.news.parsed.providers.entity_service import EntityServiceProvider
 from arekit.common.news.parsed.providers.text_opinion_pairs import TextOpinionPairsProvider
 from arekit.common.news.parsed.service import ParsedNewsService
 from arekit.common.opinions.annot.base import BaseAnnotator
-from arekit.common.opinions.base import Opinion
 from arekit.common.pipeline.item_map import MapPipelineItem
 from arekit.common.pipeline.items.flatten import FlattenIterPipelineItem
-from arekit.common.text_opinions.base import TextOpinion
+from arekit.contrib.utils.pipelines.annot.utils import iter_opinions_as_text_opinion_linkages
 
 
 def ppl_text_ids_to_parsed_news(parse_news_func):
@@ -48,7 +46,7 @@ def ppl_parsed_news_to_opinion_linkages(value_to_group_id_func, terms_per_contex
             data[1])),
 
         # (opins_provider, entities_provider, opinions) -> linkages[].
-        MapPipelineItem(map_func=lambda data: __to_text_opinion_linkages(
+        MapPipelineItem(map_func=lambda data: iter_opinions_as_text_opinion_linkages(
             provider=data[0].get_provider(TextOpinionPairsProvider.NAME),
             opinions=data[1],
             # Assign parsed news.
@@ -61,32 +59,3 @@ def ppl_parsed_news_to_opinion_linkages(value_to_group_id_func, terms_per_contex
         # linkages[] -> linkages.
         FlattenIterPipelineItem()
     ]
-
-
-def __to_text_opinion_linkages(provider, opinions, tag_value_func, filter_func):
-    assert(isinstance(provider, TextOpinionPairsProvider))
-    assert(callable(tag_value_func))
-    assert(callable(filter_func))
-
-    for opinion in opinions:
-        assert(isinstance(opinion, Opinion))
-
-        text_opinions = []
-
-        for text_opinion in provider.iter_from_opinion(opinion):
-            assert(isinstance(text_opinion, TextOpinion))
-
-            if not filter_func(text_opinion):
-                continue
-
-            text_opinions.append(text_opinion)
-
-        if len(text_opinions) == 0:
-            continue
-
-        linkage = TextOpinionsLinkage(text_opinions)
-
-        if tag_value_func is not None:
-            linkage.set_tag(tag_value_func(linkage))
-
-        yield linkage
