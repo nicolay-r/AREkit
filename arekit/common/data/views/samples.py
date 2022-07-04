5728d33732c4839b1dc133fd5e9299bcc15f112d
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-07-04 11:01:29 +0300

    #356 fix

--- arekit/common/data/views/samples.py
+++ arekit/common/data/views/samples.py
@@ -31,21 +31,19 @@ class BaseSampleStorageView(object):
 
         linked = []
 
-        current_doc_id = undefined
         current_opinion_id = undefined
 
         for row_index, sample_id in enumerate(self._storage.iter_column_values(const.ID)):
             sample_id = str(sample_id)
 
-            doc_id = self._storage.get_cell(row_index=row_index, column_name=const.DOC_ID)
             opinion_id = self.__row_ids_provider.parse_opinion_in_sample_id(sample_id)
 
-            if current_doc_id != undefined and current_opinion_id != undefined:
-                if doc_id != current_doc_id or opinion_id != current_opinion_id:
+            if current_opinion_id != undefined:
+                if opinion_id != current_opinion_id:
                     yield linked
                     linked = []
+                    current_opinion_id = opinion_id
             else:
-                current_doc_id = doc_id
                 current_opinion_id = opinion_id
 
             linked.append(self._storage.get_row(row_index))
