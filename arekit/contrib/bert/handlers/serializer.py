1da8051d769f79b621dd4bfe1941e31a3ec27701
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-07-06 15:44:21 +0300

    #322 related. Update BERT-serializer API. Support multiple pipelines.

--- arekit/contrib/bert/handlers/serializer.py
+++ arekit/contrib/bert/handlers/serializer.py
@@ -7,23 +7,24 @@ from arekit.contrib.utils.serializer import InputDataSerializationHelper
 
 class BertExperimentInputSerializerIterationHandler(ExperimentIterationHandler):
 
-    def __init__(self, pipeline, sample_rows_provider, exp_io, exp_ctx, doc_ops,
-                 save_labels_func, balance_train_samples, data_types):
+    def __init__(self, data_type_pipelines, sample_rows_provider, exp_io,
+                 exp_ctx, doc_ops, save_labels_func, balance_train_samples):
         """ sample_rows_formatter:
                 how we format input texts for a BERT model, for example:
                     - single text
                     - two sequences, separated by [SEP] token
-            pipeline:
-                doc_id -> parsed_news -> annot -> opinion linkages
-                for example, function: sentiment_attitude_extraction_default_pipeline
 
             save_labels_func: function
                 data_type -> bool
 
-            data_types: list
-                data_types, for which the data will be generated; required for:
-                    - document ids
-                    - for the result filenames.
+            data_type_pipelines: dict of, for example:
+                {
+                    DataType.Train: BasePipeline,
+                    DataType.Test: BasePipeline
+                }
+
+                pipeline: doc_id -> parsed_news -> annot -> opinion linkages
+                    for example, function: sentiment_attitude_extraction_default_pipeline
         """
         assert(isinstance(exp_io, BaseIOUtils))
         assert(isinstance(doc_ops, DocumentOperations))
@@ -34,17 +35,16 @@ class BertExperimentInputSerializerIterationHandler(ExperimentIterationHandler):
         self.__exp_io = exp_io
         self.__exp_ctx = exp_ctx
         self.__doc_ops = doc_ops
-        self.__pipeline = pipeline
-        self.__data_types = data_types
+        self.__data_type_pipelines = data_type_pipelines
         self.__save_labels_func = save_labels_func
 
     # region private methods
 
-    def __handle_iteration(self, data_type):
+    def __handle_iteration(self, data_type, pipeline):
         assert(isinstance(data_type, DataType))
 
         InputDataSerializationHelper.serialize(
-            pipeline=self.__pipeline,
+            pipeline=pipeline,
             exp_io=self.__exp_io,
             iter_doc_ids_func=lambda dtype: self.__doc_ops.iter_doc_ids(dtype),
             keep_labels_func=lambda dtype: self.__save_labels_func(data_type),
@@ -59,7 +59,7 @@ class BertExperimentInputSerializerIterationHandler(ExperimentIterationHandler):
     def on_iteration(self, iter_index):
         """ Performing data serialization for a particular iteration
         """
-        for data_type in self.__data_types:
-            self.__handle_iteration(data_type)
+        for data_type, pipeline in self.__data_type_pipelines.items():
+            self.__handle_iteration(data_type=data_type, pipeline=pipeline)
 
     # endregion
