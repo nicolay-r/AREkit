a40ccdfcc3550064ff917b71c8621581a1bb02d5
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-07-09 10:11:55 +0300

    #353 related fix

--- arekit/common/folding/united.py
+++ arekit/common/folding/united.py
@@ -5,22 +5,10 @@ class UnitedFolding(BaseDataFolding):
 
     def __init__(self, foldings):
         assert(isinstance(foldings, list))
-
-        states_count = None
-
-        # Guarantee exact the same states count as passed parameter.
-        for folding in foldings:
-            assert(isinstance(folding, BaseDataFolding))
-            if states_count is None:
-                states_count = folding._states
-            assert(folding._states == states_count)
-
         self.__foldings = foldings
-
         super(UnitedFolding, self).__init__(
             doc_ids_to_fold=UnitedFolding.__iter_all_doc_ids(foldings),
-            supported_data_types=list(set(UnitedFolding.__iter_all_data_types(foldings))),
-            states_count=states_count)
+            supported_data_types=list(set(UnitedFolding.__iter_all_data_types(foldings))))
 
     @property
     def Name(self):
@@ -60,10 +48,3 @@ class UnitedFolding(BaseDataFolding):
             self.__merge(origin=origin, new_data=new_data)
 
         return origin
-
-    def _assign_index(self, i):
-        """ Update current index for every folding.
-        """
-        for folding in self.__foldings:
-            assert(isinstance(folding, BaseDataFolding))
-            folding._assign_index(i)
