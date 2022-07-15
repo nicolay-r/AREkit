b3579294c2d2da79cbbf8d6e6733d796be29e954
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-07-15 11:26:05 +0300

    Removed non utilized import

--- arekit/contrib/utils/pipelines/opinion_collections.py
+++ arekit/contrib/utils/pipelines/opinion_collections.py
@@ -6,7 +6,6 @@ from arekit.common.model.labeling.modes import LabelCalculationMode
 from arekit.common.model.labeling.single import SingleLabelsHelper
 from arekit.common.opinions.base import Opinion
 from arekit.common.opinions.collection import OpinionCollection
-from arekit.common.pipeline.base import BasePipeline
 from arekit.common.pipeline.item_map import MapPipelineItem
 from arekit.common.pipeline.items.iter import FilterPipelineItem
 
