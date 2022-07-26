25b9ecf8af54c40119ea6f4a918723a61a53af4f
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-07-26 11:01:26 +0300

    #377 related fix

--- arekit/contrib/bert/terms/mapper.py
+++ arekit/contrib/bert/terms/mapper.py
@@ -6,4 +6,15 @@ class BertDefaultStringTextTermsMapper(OpinionContainingTextTermsMapper):
         relies on hidden states of language models. By default implemenation of
         a base class assumes to provide an orginal frame variant value.
     """
-    pass
+    
+    def __init__(self, entity_formatter, word_separator=' '):
+        """ See https://github.com/nicolay-r/AREkit/issues/377
+            for a greater details.
+        """
+        super(BertDefaultStringTextTermsMapper, self).__init__(entity_formatter)
+        self.__word_separator = word_separator
+
+    def map_entity(self, e_ind, entity):
+        # guarantee that the base entity will be presented as a single term.
+        entity_value = super(BertDefaultStringTextTermsMapper, self).map_entity(e_ind=e_ind, entity=entity)
+        return entity_value.replace(self.__word_separator, '-')
