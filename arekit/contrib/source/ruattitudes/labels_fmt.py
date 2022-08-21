f6500265597930f3704df5fed4e38f3353caf926
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-08-21 11:32:58 +0300

    Fixed: switched to type instead of instance

--- arekit/contrib/source/ruattitudes/labels_fmt.py
+++ arekit/contrib/source/ruattitudes/labels_fmt.py
@@ -8,5 +8,5 @@ class RuAttitudesLabelFormatter(StringLabelsFormatter):
         assert(isinstance(label_scaler, BaseLabelScaler))
         stol = {}
         for uint_label in [0, 1, 2]:
-            stol[str(uint_label)] = label_scaler.uint_to_label(uint_label)
+            stol[str(uint_label)] = type(label_scaler.uint_to_label(uint_label))
         super(RuAttitudesLabelFormatter, self).__init__(stol=stol)
