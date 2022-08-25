25ea425a8cafcc260ad26a158ee9219ca66d6944
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-08-25 15:43:19 +0300

    Rename parameter names

--- arekit/common/folding/nofold.py
+++ arekit/common/folding/nofold.py
@@ -5,12 +5,9 @@ class NoFolding(BaseDataFolding):
     """ The case of absent folding in experiment.
     """
 
-    def __init__(self, doc_ids_to_fold, supported_data_types):
-        if len(supported_data_types) > 1:
-            raise NotImplementedError("Experiments with such amount of data-types are not supported!")
-
-        super(NoFolding, self).__init__(doc_ids_to_fold=doc_ids_to_fold,
-                                        supported_data_types=supported_data_types)
+    def __init__(self, doc_ids, supported_data_type):
+        super(NoFolding, self).__init__(doc_ids_to_fold=doc_ids,
+                                        supported_data_types=[supported_data_type])
 
     def fold_doc_ids_set(self):
         return {
