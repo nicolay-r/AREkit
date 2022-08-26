cd303cd829cf5d22840b429f47321996e9cba36c
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-08-26 13:48:40 +0300

    Removed non utilized method

--- arekit/common/entities/str_fmt.py
+++ arekit/common/entities/str_fmt.py
@@ -6,8 +6,3 @@ class StringEntitiesFormatter(object):
     def to_string(self, original_value, entity_type):
         assert(isinstance(entity_type, OpinionEntityType))
         raise NotImplementedError()
-
-    @staticmethod
-    def iter_supported_types():
-        for entity_type in OpinionEntityType:
-            yield entity_type
