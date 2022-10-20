4be089559a4272386e334f3bd34efd21562fbca1
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-10-20 15:05:25 +0300

    #410 -- fixed incorrect method name.

--- arekit/contrib/utils/data/writers/json_opennre.py
+++ arekit/contrib/utils/data/writers/json_opennre.py
@@ -35,7 +35,7 @@ class OpenNREJsonWriter(BaseWriter):
         json.dump(bag, json_file, separators=(",", ":"), ensure_ascii=False)
         json_file.write("\n")
 
-    def save(self, storage, target):
+    def write(self, storage, target):
         assert(isinstance(storage, BaseRowsStorage))
         assert(isinstance(target, str))
 
