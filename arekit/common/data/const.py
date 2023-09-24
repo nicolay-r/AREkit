8fe24082f9924cf52be6a2affbbc826e8eef3e4c
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-09-24 12:53:28 +0100

    #488 removed non-utilized constants

--- arekit/common/data/const.py
+++ arekit/common/data/const.py
@@ -8,10 +8,6 @@ LABEL_STR = 'label_str'
 OPINION_ID = "opinion_id"
 OPINION_LINKAGE_ID = "linkage_id"
 
-# Corresponds to fields with attitude ends. (values, STRING)
-SOURCE = 'source'
-TARGET = 'target'
-
 # Corresponds to fields with attitude ends. (indices, INT)
 S_IND = 's_ind'
 T_IND = 't_ind'
