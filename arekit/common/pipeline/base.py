cf13a66b289320c0e04d5bc939b0dabf75f515b1
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-01-08 09:51:02 +0000

    #546 support

--- arekit/common/pipeline/base.py
+++ arekit/common/pipeline/base.py
@@ -5,14 +5,16 @@ from arekit.common.pipeline.items.base import BasePipelineItem
 class BasePipelineLauncher:
 
     @staticmethod
-    def run(pipeline, pipeline_ctx, src_key=None):
+    def run(pipeline, pipeline_ctx, src_key=None, has_input=True):
         assert(isinstance(pipeline, list))
         assert(isinstance(pipeline_ctx, PipelineContext))
         assert(isinstance(src_key, str) or src_key is None)
 
         for ind, item in enumerate(filter(lambda itm: itm is not None, pipeline)):
             assert(isinstance(item, BasePipelineItem))
-            input_data = item.get_source(pipeline_ctx, force_key=src_key if src_key is not None and ind == 0 else None)
+            do_force_key = src_key is not None and ind == 0
+            input_data = item.get_source(pipeline_ctx, force_key=src_key if do_force_key else None) \
+                if has_input and ind == 0 else None
             item_result = item.apply(input_data=input_data, pipeline_ctx=pipeline_ctx)
             pipeline_ctx.update(param=item.ResultKey, value=item_result, is_new_key=False)
 
