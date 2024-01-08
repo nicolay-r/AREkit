c0d600b56d73ad77ae9b50a94c66d42c1825ba5a
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-01-08 21:38:13 +0000

    #547 done

--- arekit/contrib/utils/io_utils/samples.py
+++ arekit/contrib/utils/io_utils/samples.py
@@ -1,10 +1,8 @@
 import logging
-from os.path import join
 
 from arekit.contrib.utils.data.readers.base import BaseReader
 from arekit.common.experiment.api.base_samples_io import BaseSamplesIO
 from arekit.contrib.utils.data.writers.base import BaseWriter
-from arekit.contrib.utils.io_utils.utils import check_targets_existence
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -13,31 +11,24 @@ logging.basicConfig(level=logging.INFO)
 class SamplesIO(BaseSamplesIO):
     """ Samples default IO utils for samples.
             Sample is a text part which include pair of attitude participants.
-            This class allows to provide saver and loader for such entries, bubbed as samples.
+            This class allows to provide saver and loader for such entries, dubbed as samples.
             Samples required for machine learning training/inferring.
     """
 
-    def __init__(self, target_dir, writer=None, reader=None, prefix="sample"):
-        assert(isinstance(target_dir, str))
-        assert(isinstance(prefix, str))
+    def __init__(self, create_target_func, writer=None, reader=None):
         assert(isinstance(writer, BaseWriter) or writer is None)
         assert(isinstance(reader, BaseReader) or reader is None)
-        self.__target_dir = target_dir
-        self.__prefix = prefix
+        assert(callable(create_target_func))
+
         self.__writer = writer
         self.__reader = reader
+        self.__create_target_func = create_target_func
 
         self.__target_extension = None
         if writer is not None:
             self.__target_extension = writer.extension()
         elif reader is not None:
             self.__target_extension = reader.extension()
-
-    # region public methods
-
-    @property
-    def Prefix(self):
-        return self.__prefix
 
     @property
     def Reader(self):
@@ -48,31 +39,4 @@ class SamplesIO(BaseSamplesIO):
         return self.__writer
 
     def create_target(self, data_type):
-        return self.__get_input_sample_target(data_type)
-
-    def check_targets_existed(self, data_types_iter):
-        for data_type in data_types_iter:
-
-            targets = [
-                self.__get_input_sample_target(data_type=data_type),
-            ]
-
-            if not check_targets_existence(targets=targets):
-                return False
-        return True
-
-    # endregion
-
-    def __get_input_sample_target(self, data_type):
-        return self.__get_filepath(out_dir=self.__target_dir,
-                                   template=f"{data_type.name.lower()}",
-                                   prefix=self.__prefix,
-                                   extension=self.__target_extension)
-
-    @staticmethod
-    def __get_filepath(out_dir, template, prefix, extension):
-        assert(isinstance(template, str))
-        assert(isinstance(prefix, str))
-        assert(isinstance(extension, str))
-        return join(out_dir, "{prefix}-{template}{extension}".format(
-            prefix=prefix, template=template, extension=extension))
+        return self.__create_target_func(data_type) + self.__target_extension
--- arekit/contrib/utils/io_utils/utils.py
+++ arekit/contrib/utils/io_utils/utils.py
@@ -1,20 +1,10 @@
 from collections.abc import Iterable
 import logging
-from os.path import join, exists
+from os.path import exists
 
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-
-
-def join_dir_with_subfolder_name(subfolder_name, dir):
-    """ Returns subfolder in in directory
-    """
-    assert(isinstance(subfolder_name, str))
-    assert(isinstance(dir, str))
-
-    target_dir = join(dir, "{}/".format(subfolder_name))
-    return target_dir
 
 
 def check_targets_existence(targets):
